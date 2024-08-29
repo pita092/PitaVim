4f96f4390de30215c74cfd09066f399765520f2f
pita <itaymalka37@gmail.com>
2024-08-30 00:38:28 +0300

    s

---
+++ lua/pitavim/plugins/init.lua
@@ -0,0 +1,19 @@
+return{
+{"nvim-lua/plenary.nvim",}
+{"nvim-tree/nvim-web-devicons",}
+{"nvim-treesitter/nvim-treesitter",}
+{"NvChad/nvim-colorizer.lua",}
+{
+"nvim-telescope/telescope.nvim",
+}
+{
+    "lukas-reineke/indent-blankline.nvim",
+    event = "User FilePost",
+    opts = {
+      indent = { char = "│", highlight = "IblChar" },
+      scope = { char = "│", highlight = "IblScopeChar" },
+    },
+    config = function()
+    end,
+  },
+}
