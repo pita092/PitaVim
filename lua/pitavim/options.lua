c1b5334079da14423e9e5bc66a9e9d8871c86da8
pita <itaymalka37@gmail.com>
2024-08-30 01:54:45 +0300

    s

---
+++ lua/pitavim/options.lua
@@ -0,0 +1,40 @@
+vim.cmd 'set number'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set shiftwidth=2'
+vim.g.have_nerd_font = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+im.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+--vim.opt.listchars = {tab = "» " , trail = "·", nbsp = "␣" }
+vim.opt.listchars = {
+  eol = '↵',
+  tab = '· ',
+  trail = '▸',
+  extends = '❯',
+  precedes = '❮',
+}
+vim.opt.list = true
+vim.o.showtabline = 2
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+opt.whichwrap:append "<>[]hl"
+local is_windows = vim.fn.has "win32" ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
