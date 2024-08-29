0568cb5e891279d321c934ad1b984cab3dc0db52
pita <itaymalka37@gmail.com>
2024-08-30 00:54:55 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,4 +1,16 @@
 
 return {
     "nvim-tree/nvim-tree.lua",
+    {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    opts = function()
+      return require "nvchad.configs.treesitter"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
     }
