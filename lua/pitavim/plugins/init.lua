63a5a58f6b8f48778d87738b04902dbe83782568
pita <itaymalka37@gmail.com>
2024-08-30 00:41:19 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,11 +1,11 @@
 return{
-{"nvim-lua/plenary.nvim",}
-{"nvim-tree/nvim-web-devicons",}
-{"nvim-treesitter/nvim-treesitter",}
-{"NvChad/nvim-colorizer.lua",}
+{"nvim-lua/plenary.nvim",},
+{"nvim-tree/nvim-web-devicons",},
+{"nvim-treesitter/nvim-treesitter",},
+{"NvChad/nvim-colorizer.lua",},
 {
 "nvim-telescope/telescope.nvim",
-}
+},
 {
     "lukas-reineke/indent-blankline.nvim",
     event = "User FilePost",
