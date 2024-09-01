ab100638110c5b0bc62b6dfc09dc074d2759ad09
pita <itaymalka37@gmail.com>
2024-09-01 20:00:22 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -120,7 +120,7 @@ return {
   },
   {
     "ellisonleao/gruvbox.nvim",
-    event = "VeryLazy",
+    event ="BufReadPre",
     priority = 1000,
     config = function()
       return require("pitavim.configs.gruvbox")
