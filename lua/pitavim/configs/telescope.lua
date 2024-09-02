1bf77493efb8e41fdd82f78a3fa4be5898c26c35
pita <itaymalka37@gmail.com>
2024-09-02 23:43:08 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -1,39 +1,39 @@
 require('telescope').setup {
 
-	defaults = {
-		layout_strategy = 'vertical',
-		layout_config = {
-			width = 0.8,
-			height = 0.9,
-			preview_cutoff = 120,
-		},
-		borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-		color_devicons = true,
-	},
-	pickers = {
-		find_files = {
-			theme = 'dropdown',
-		},
-	},
-	extensions = {
-		['ui-select'] = {
-			require('telescope.themes').get_dropdown(),
-			fzf = {
-				fuzzy = true,
-				override_generic_sorter = true,
-				override_file_sorter = true,
-				case_mode = 'smart_case',
-			},
-		},
-		media_files = {
-			filetypes = { 'png', 'webp', 'jpg', 'jpeg' },
-			find_cmd = 'rg',
-		},
-	},
-	cond = function()
-		return vim.fn.executable 'make' == 1
-	end,
-	winblend = 30,
+  defaults = {
+    layout_strategy = "horizontal",
+    layout_config = {
+      width = 0.8,
+      height = 0.8,
+      preview_cutoff = 120,
+    },
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+  },
+  pickers = {
+    find_files = {
+      theme = 'dropdown',
+    },
+  },
+  extensions = {
+    ['ui-select'] = {
+      require('telescope.themes').get_dropdown(),
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = 'smart_case',
+      },
+    },
+    media_files = {
+      filetypes = { 'png', 'webp', 'jpg', 'jpeg' },
+      find_cmd = 'rg',
+    },
+  },
+  cond = function()
+    return vim.fn.executable 'make' == 1
+  end,
+  winblend = 30,
 }
 require('telescope').load_extension 'ui-select'
 
