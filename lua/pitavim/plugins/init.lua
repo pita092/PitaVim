9699e337ab0e81918158d51155e48200b01eaa6e
pita <itaymalka37@gmail.com>
2024-09-03 00:30:40 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -69,7 +69,8 @@ return {
       "nvim-telescope/telescope-ui-select.nvim",
     },
     {
-      "nvim-telescope/telescope-fzf-native.nvim"
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
     },
   },
   {
