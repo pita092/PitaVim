b2e1b2741ce65a88d9211ae39b8124ed2b199bb9
pita <itaymalka37@gmail.com>
2024-09-03 14:14:04 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -20,19 +20,6 @@ return {
     end
   },
   {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    event = 'VeryLazy',
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    config = function()
-      return require("pitavim.configs.winbar")
-    end
-  },
-  {
     "folke/which-key.nvim",
     keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
     event = "VeryLazy",
