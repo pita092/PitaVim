49d88c85dd72a09f2d0989730c0ce3d0ea8fac28
pita <itaymalka37@gmail.com>
2024-09-03 14:02:47 +0300

    s

--- lua/pitavim/configs/lualine.lua
+++ lua/pitavim/configs/lualine.lua
@@ -23,7 +23,7 @@ require('lualine').setup {
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_z = { 'g:CmpStatus' }
   },
   inactive_sections = {
     lualine_a = {},
