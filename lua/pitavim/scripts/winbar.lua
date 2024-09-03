96db07bbf00bde8e8d634ae31c31764872c1c3bf
pita <itaymalka37@gmail.com>
2024-09-03 14:08:09 +0300

    s

--- lua/pitavim/scripts/winbar.lua
+++ lua/pitavim/scripts/winbar.lua
@@ -1,18 +1,17 @@
-vim.o.winbar = "%{%v:lua.require'custom.winbar'.get_winbar()%}"
-
 -- In custom/winbar.lua
 local M = {}
 
 function M.get_winbar()
-    local file_name = vim.fn.expand("%:t")
-    local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-    
-    if branch ~= "" then
-        return string.format("%s   %s", file_name, branch)
-    else
-        return file_name
-    end
+  local file_name = vim.fn.expand("%:t")
+  local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+
+  if branch ~= "" then
+    return string.format("%s   %s", file_name, branch)
+  else
+    return file_name
+  end
 end
 
-return M
+vim.o.winbar = "%{%v:lua.require'custom.winbar'.get_winbar()%}"
 
+return M
