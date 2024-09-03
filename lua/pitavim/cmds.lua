a830458e3531853bfe490a8d29b2016c0ee7e175
pita <itaymalka37@gmail.com>
2024-09-03 12:29:55 +0300

    s

--- lua/pitavim/cmds.lua
+++ lua/pitavim/cmds.lua
@@ -45,3 +45,9 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "#83a598" })
   end,
 })
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    boo.boo()
+  end
+})
