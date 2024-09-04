38858036e43e236337f302e0f65cac974de001ab
pita <itaymalka37@gmail.com>
2024-09-04 19:48:02 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -1,44 +1,48 @@
 require("telescope").setup({
-  defaults = {
-    layout_strategy = "horizontal",
-    layout_config = {
-      width = 0.8,
-      height = 0.8,
-      prompt_position = "top",
-    },
-    prompt_prefix = " 🔭 ",
-    entry_prefix = " ● ",
-    selection_caret = "  ● ",
-    path_display = { "truncate" },
-    file_ignore_patterns = {
-      previewer = false,
-      "dist",
-      "target",
-      "node_modules",
-      "pack/plugins",
-    },
-  },
-  pickers = {
-    find_files = {},
-  },
-  extensions = {
-    ["ui-select"] = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
-      },
-    },
-    media_files = {
-      filetypes = { "png", "webp", "jpg", "jpeg" },
-      find_cmd = "rg",
-    },
-  },
-  cond = function()
-    return vim.fn.executable("make") == 1
-  end,
-  winblend = 30,
+	defaults = {
+		-- layout_strategy = "horizontal",
+		-- layout_config = {
+		--   width = 0.8,
+		--   height = 0.8,
+		--   prompt_position = "top",
+		layout_strategy = "center",
+		layout_config = {
+			width = 0.5,
+			height = 0.4,
+		},
+		prompt_prefix = " 🔭 ",
+		entry_prefix = " ● ",
+		selection_caret = "  ● ",
+		path_display = { "truncate" },
+		file_ignore_patterns = {
+			previewer = false,
+			"dist",
+			"target",
+			"node_modules",
+			"pack/plugins",
+		},
+	},
+	pickers = {
+		find_files = {},
+	},
+	extensions = {
+		["ui-select"] = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+		},
+		media_files = {
+			filetypes = { "png", "webp", "jpg", "jpeg" },
+			find_cmd = "rg",
+		},
+	},
+	cond = function()
+		return vim.fn.executable("make") == 1
+	end,
+	winblend = 30,
 })
 require("telescope").load_extension("ui-select")
 require("telescope").load_extension("fzf")
