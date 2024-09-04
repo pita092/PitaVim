f8e208cf3e56358c43dae3862616fa6fc0e21d53
pita <itaymalka37@gmail.com>
2024-09-04 22:58:56 +0300

    s

--- lua/pitavim/maps.lua
+++ lua/pitavim/maps.lua
@@ -27,13 +27,13 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>H", builtin.help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>K", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>o", butltin.live_grep, { desc = "[O] Live grep" })
+vim.keymap.set("n", "<leader>o", builtin.live_grep, { desc = "[O] Live grep" })
 vim.keymap.set("n", "<leader>s", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 vim.keymap.set("n", "<leader>r", builtin.resume, { desc = "[S]earch [R]esume" })
 vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 vim.keymap.set("n", "<leader>p", function()
-  builtin.butltin(require("telescope.themes").get_dropdown({
+  builtin.builtin(require("telescope.themes").get_dropdown({
     winblend = 0,
     previewer = false,
   }))
