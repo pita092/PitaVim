e76360effd7598f0379b9ecc490f58e877e9ebc0
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 00:25:13 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -17,7 +17,7 @@ require("telescope").setup({
     initial_mode = "insert",
     selection_strategy = "reset",
     sorting_strategy = "ascending",
-    layout_strategy = "vertical",
+    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
         prompt_position = "top",
