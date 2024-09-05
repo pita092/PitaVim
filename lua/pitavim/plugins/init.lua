a7c4f24be20a2d1e795a8157954bc1e1af37c89d
pita <154020297+pita092@users.noreply.github.com>
2024-09-05 21:38:45 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -164,7 +164,7 @@ return {
   },
   {
     "stevearc/oil.nvim",
-    cmd = "Oil",
+    Lazy = false,
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     config = function()
       return require("pitavim.configs.oil")
