3b73dbdc4d1f4ab220c8ad1c25a867856f586cbb
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 00:42:31 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -12,7 +12,7 @@ require("telescope").setup({
       "--smart-case",
     },
     prompt_prefix = "   ",
-    selection_caret = "🚀",
+    selection_caret = "🚀 ",
     entry_prefix = "  ",
     initial_mode = "insert",
     selection_strategy = "reset",
