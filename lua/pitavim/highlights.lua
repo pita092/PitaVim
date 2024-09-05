6c55c4a6bb0fc211e65ce69f8e534fcadecfd98d
pita <154020297+pita092@users.noreply.github.com>
2024-09-05 23:59:45 +0300

    s

---
+++ lua/pitavim/highlights.lua
@@ -0,0 +1,23 @@
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatNormal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "NONE" })
+return {
+  TelescopeBorder = { fg = #262626, bg = #282828 },
+  TelescopeNormal = { bg = #282828 },
+  TelescopePreviewBorder = { fg = #282828, bg = #282828 },
+  TelescopePreviewNormal = { bg = #282828 },
+  TelescopePreviewTitle = { fg = #282828, bg = #fbf1c7 },
+  TelescopePromptBorder = { fg = #262626, bg = #262626 },
+  TelescopePromptNormal = { fg = #fbf1c7, bg = #262626 },
+  TelescopePromptPrefix = { fg = #fbf1c7, bg = #262626 },
+  TelescopePromptTitle = { fg = #fbf1c7, bg = #282828 },
+  TelescopeResultsBorder = { fg = #fbf1c7, bg = #282828 },
+  TelescopeResultsNormal = { bg = #282828 },
+  TelescopeResultsTitle = { fg = #fbf1c7, bg = #282828 },
+}
