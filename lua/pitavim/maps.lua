ea97f9692cd8e52de6218678e5956cb87ca70053
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 01:11:20 +0300

    s

--- lua/pitavim/maps.lua
+++ lua/pitavim/maps.lua
@@ -21,8 +21,8 @@ map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP Diagnostic locli
 
 -- neotree/oil
 map("n", "<leader>e", ":Neotree reveal right<CR>", { desc = "neotree reavel" })
-map("n", "<C-d>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-map("n", "<C-D>", ":lua require('oil').close()<CR>", { desc = "Close oil" })
+map("n", "<C-d", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map("n", "<C-f>", ":lua require('oil').close()<CR>", { desc = "Close oil" })
 
 -- telescope
 local builtin = require("telescope.builtin")
