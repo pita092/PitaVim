0fd8e61b01112099bd946264a622e06a199fee5f
pita <154020297+pita092@users.noreply.github.com>
2024-09-05 21:35:18 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -163,6 +163,15 @@ return {
 		},
 	},
 	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
+	{
 		"NvChad/nvterm",
 		event = "BufReadPre",
 		config = function()
