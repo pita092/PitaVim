5de982cf59d174a1f7f0148a32a2cf948c6f9948
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 01:52:52 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -139,14 +139,14 @@ return {
 			return require("pitavim.configs.cmp")
 		end,
 	},
-	-- {
-	-- 	"ellisonleao/gruvbox.nvim",
-	-- 	event = "VeryLazy",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		return require("pitavim.configs.gruvbox")
-	-- 	end,
-	-- },
+	{
+		"ellisonleao/gruvbox.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			return require("pitavim.configs.gruvbox")
+		end,
+	},
 	{
 		"NvChad/nvim-colorizer.lua",
 		event = "VeryLazy",
