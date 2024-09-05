1757bae7ea5f7dea485f4061e56e42b0a3242166
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 00:07:44 +0300

    s

--- lua/pitavim/highlights.lua
+++ lua/pitavim/highlights.lua
@@ -7,17 +7,16 @@ vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NeoTreeFloatNormal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "NONE" })
-return {
-  TelescopeBorder = { fg = #262626, bg = #282828 },
-  TelescopeNormal = { bg = #282828 },
-  TelescopePreviewBorder = { fg = #282828, bg = #282828 },
-  TelescopePreviewNormal = { bg = #282828 },
-  TelescopePreviewTitle = { fg = #282828, bg = #fbf1c7 },
-  TelescopePromptBorder = { fg = #262626, bg = #262626 },
-  TelescopePromptNormal = { fg = #fbf1c7, bg = #262626 },
-  TelescopePromptPrefix = { fg = #fbf1c7, bg = #262626 },
-  TelescopePromptTitle = { fg = #fbf1c7, bg = #282828 },
-  TelescopeResultsBorder = { fg = #fbf1c7, bg = #282828 },
-  TelescopeResultsNormal = { bg = #282828 },
-  TelescopeResultsTitle = { fg = #fbf1c7, bg = #282828 },
-}
+
+vim.api.nvim_set_hl("TelescopeBorder", { fg = "#262626", bg = "#282828" })
+vim.api.nvim_set_hl("TelescopeNormal", { bg = "#282828" })
+vim.api.nvim_set_hl("TelescopePreviewBorder", { fg = "#282828", bg = "#282828" })
+vim.api.nvim_set_hl("TelescopePreviewNormal", { bg = "#282828" })
+vim.api.nvim_set_hl("TelescopePreviewTitle", { fg = "#282828", bg = "#fbf1c7" })
+vim.api.nvim_set_hl("TelescopePromptBorder", { fg = "#262626", bg = "#262626" })
+vim.api.nvim_set_hl("TelescopePromptNormal", { fg = "#fbf1c7", bg = "#262626" })
+vim.api.nvim_set_hl("TelescopePromptPrefix", { fg = "#fbf1c7", bg = "#262626" })
+vim.api.nvim_set_hl("TelescopePromptTitle", { fg = "#fbf1c7", bg = "#282828" })
+vim.api.nvim_set_hl("TelescopeResultsBorder", { fg = "#fbf1c7", bg = "#282828" })
+vim.api.nvim_set_hl("TelescopeResultsNormal", { bg = "#282828" })
+vim.api.nvim_set_hl("TelescopeResultsTitle", { fg = "#fbf1c7", bg = "#282828" })
