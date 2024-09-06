948d43e95dd5b2014afec9b0b12e8427f7b800b5
pita <154020297+pita092@users.noreply.github.com>
2024-09-06 12:33:47 +0300

    s

--- lua/pitavim/cmds.lua
+++ lua/pitavim/cmds.lua
@@ -1,34 +1,34 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
-  group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
-  callback = function(args)
-    local file = vim.api.nvim_buf_get_name(args.buf)
-    local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+	group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+	callback = function(args)
+		local file = vim.api.nvim_buf_get_name(args.buf)
+		local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
 
-    if not vim.g.ui_entered and args.event == "UIEnter" then
-      vim.g.ui_entered = true
-    end
+		if not vim.g.ui_entered and args.event == "UIEnter" then
+			vim.g.ui_entered = true
+		end
 
-    if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
-      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
-      vim.api.nvim_del_augroup_by_name("NvFilePost")
+		if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+			vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+			vim.api.nvim_del_augroup_by_name("NvFilePost")
 
-      vim.schedule(function()
-        vim.api.nvim_exec_autocmds("FileType", {})
+			vim.schedule(function()
+				vim.api.nvim_exec_autocmds("FileType", {})
 
-        if vim.g.editorconfig then
-          require("editorconfig").config(args.buf)
-        end
-      end)
-    end
-  end,
+				if vim.g.editorconfig then
+					require("editorconfig").config(args.buf)
+				end
+			end)
+		end
+	end,
 })
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "neo-tree",
-  callback = function()
-    vim.opt_local.fillchars:append({ eob = " " })
-  end,
+	pattern = "neo-tree",
+	callback = function()
+		vim.opt_local.fillchars:append({ eob = " " })
+	end,
 })
 vim.cmd([[
   autocmd VimEnter * highlight clear TabLineFill
@@ -39,15 +39,30 @@ vim.cmd([[
 ]])
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#458588" })
-    vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "#83a598" })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#458588" })
+		vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "#83a598" })
+	end,
 })
 
 vim.defer_fn(function()
-  vim.cmd([[
+	vim.cmd([[
     highlight SignColumn guibg=NONE ctermbg=NONE
   ]])
 end, 100)
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Remove background from diagnostic signs
+vim.cmd([[
+  highlight DiagnosticSignError guibg=NONE
+  highlight DiagnosticSignWarn guibg=NONE
+  highlight DiagnosticSignInfo guibg=NONE
+  highlight DiagnosticSignHint guibg=NONE
+]])
