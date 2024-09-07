f1b215af23ff42f8b758564a61309cdca680bd6d
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 02:12:49 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,39 +1,13 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
+
 wilder.set_option("pipeline", {
-	wilder.branch(
-		wilder.python_file_finder_pipeline({
-			-- to use ripgrep : {'rg', '--files'}
-			-- to use fd      : {'fd', '-tf'}
-			file_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
-			-- to use fd      : {'fd', '-td'}
-			dir_command = { "find", ".", "-type", "d", "-printf", "%P\n" },
-			-- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-			-- found at https://github.com/nixprime/cpsm
-			filters = { "fuzzy_filter", "difflib_sorter" },
-		}),
-		wilder.cmdline_pipeline(),
-		wilder.python_search_pipeline()
-	),
+	wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
 })
+
 wilder.set_option(
 	"renderer",
-	wilder.popupmenu_renderer({
+	wilder.wildmenu_renderer({
 		highlighter = wilder.basic_highlighter(),
-		left = { " ", wilder.popupmenu_devicons() },
-		right = { " ", wilder.popupmenu_scrollbar() },
-	})
-)
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		highlighter = {
-			wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
-			wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
-			-- at https://github.com/romgrk/fzy-lua-native
-		},
-		highlights = {
-			accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#fabd2f" } }),
-		},
 	})
 )
