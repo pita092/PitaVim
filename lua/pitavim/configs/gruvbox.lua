cc3dfd65a57afdefba30a9eaa66e52ad7d155e2b
pita <itaymalka37@gmail.com>
2024-09-07 20:30:30 +0300

    s

--- lua/pitavim/configs/gruvbox.lua
+++ lua/pitavim/configs/gruvbox.lua
@@ -1,4 +1,7 @@
 require("gruvbox").setup({
+  palette_overrides = {
+    dark0 - "#282828",
+  },
   terminal_colors = true, -- add neovim terminal colors
   undercurl = true,
   underline = true,
@@ -15,8 +18,8 @@ require("gruvbox").setup({
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
+  inverse = true,   -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
