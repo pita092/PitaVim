329c6b360019d20ac6468aa05ee9a30c7d288863
pita <itaymalka37@gmail.com>
2024-09-07 20:51:39 +0300

    s

--- lua/pitavim/cmds.lua
+++ lua/pitavim/cmds.lua
@@ -49,3 +49,10 @@ vim.cmd([[
   colorscheme gruvbox
   highlight Normal guibg=#282828
   ]])
+
+vim.cmd([[
+  sign define DiagnosticSignError text=● texthl=DiagnosticSignError linehl= numhl=
+  sign define DiagnosticSignWarn text=● texthl=DiagnosticSignWarn linehl= numhl=
+  sign define DiagnosticSignInfo text=● texthl=DiagnosticSignInfo linehl= numhl=
+  sign define DiagnosticSignHint text=● texthl=DiagnosticSignHint linehl= numhl=
+]])
