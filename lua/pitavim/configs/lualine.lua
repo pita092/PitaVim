1a2314c6a9a28f461ccdbfab5c72e2143127d5ea
pita <itaymalka37@gmail.com>
2024-09-07 20:57:27 +0300

    s

--- lua/pitavim/configs/lualine.lua
+++ lua/pitavim/configs/lualine.lua
@@ -1,122 +1,40 @@
--- require("lualine").setup({
--- 	options = {
--- 		icons_enabled = true,
--- 		theme = "gruvbox",
--- 		component_separators = { left = "", right = "" },
--- 		section_separators = { left = "", right = "" },
--- 		disabled_filetypes = {
--- 			statusline = {},
--- 			winbar = {},
--- 		},
--- 		ignore_focus = {},
--- 		always_divide_middle = true,
--- 		globalstatus = false,
--- 		refresh = {
--- 			statusline = 1000,
--- 			tabline = 1000,
--- 			winbar = 1000,
--- 		},
--- 	},
--- 	sections = {
--- 		lualine_a = { "mode" },
--- 		lualine_b = { "branch", "diff", "diagnostics" },
--- 		lualine_c = { "filename" },
--- 		lualine_x = { "encoding", "fileformat", "filetype" },
--- 		lualine_y = { "progress" },
--- 		lualine_z = {},
--- 	},
--- 	inactive_sections = {
--- 		lualine_a = {},
--- 		lualine_b = {},
--- 		lualine_c = { "filename" },
--- 		lualine_x = { "location" },
--- 		lualine_y = {},
--- 		lualine_z = {},
--- 	},
--- 	tabline = {},
--- 	winbar = {},
--- 	inactive_winbar = {},
--- 	extensions = {},
--- })
-local function diff_source()
-	local gitsigns = vim.b.gitsigns_status_dict
-	if gitsigns then
-		return {
-			added = gitsigns.added,
-			modified = gitsigns.changed,
-			removed = gitsigns.removed,
-		}
-	end
-end
-
-local function lsp_progress()
-	local messages = vim.lsp.util.get_progress_messages()
-	if #messages == 0 then
-		return ""
-	end
-	local status = {}
-	for _, msg in pairs(messages) do
-		table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
-	end
-	return table.concat(status, " | ")
-end
-
--- Then in your lualine setup:
-
-local colors = {
-	blue = "#80a0ff",
-	cyan = "#79dac8",
-	black = "#080808",
-	white = "#c6c6c6",
-	red = "#ff5189",
-	violet = "#d183e8",
-	grey = "#303030",
-}
-
-local bubbles_theme = {
-	normal = {
-		a = { fg = colors.black, bg = colors.violet },
-		b = { fg = colors.white, bg = colors.grey },
-		c = { fg = colors.white, bg = colors.black },
-	},
-
-	insert = { a = { fg = colors.black, bg = colors.blue } },
-	visual = { a = { fg = colors.black, bg = colors.cyan } },
-	replace = { a = { fg = colors.black, bg = colors.red } },
-
-	inactive = {
-		a = { fg = colors.white, bg = colors.black },
-		b = { fg = colors.white, bg = colors.black },
-		c = { fg = colors.black, bg = colors.black },
-	},
-}
-
 require("lualine").setup({
 	options = {
-		theme = bubbles_theme,
-		component_separators = "|",
+		icons_enabled = true,
+		theme = "gruvbox",
+		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
 	},
 	sections = {
-		lualine_a = {
-			{ "mode", separator = { left = "" }, right_padding = 2 },
-		},
-		lualine_b = { "filename", "branch" },
-		lualine_c = { "fileformat" },
-		lualine_x = {},
-		lualine_y = { "filetype", "progress" },
-		lualine_z = {
-			{ "location", separator = { right = "" }, left_padding = 2 },
-		},
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = {},
 	},
 	inactive_sections = {
-		lualine_a = { "filename" },
+		lualine_a = {},
 		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
 		lualine_y = {},
-		lualine_z = { "location" },
+		lualine_z = {},
 	},
 	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
 	extensions = {},
 })
