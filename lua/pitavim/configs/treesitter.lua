fdea87eebfbcd48afb0f0b27e1af89835f37862d
pita <itaymalka37@gmail.com>
2024-09-07 21:46:08 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -1,15 +1,40 @@
+-- local options = {
+--   ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+--
+--   highlight = {
+--     enable = true,
+--     use_languagetree = true,
+--   },
+--
+--   indent = { enable = true },
+--   vim.filetype.add({
+--     pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+--   })
+-- }
+
 local options = {
-  ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
-
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
-
-  indent = { enable = true },
-  vim.filetype.add({
-    pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-  })
+	highlight = { enable = true },
+	indent = { enable = true },
+	sync_install = false,
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
+	textobjects = {
+		move = {
+			enable = true,
+			goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+			goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+			goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+			goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+		},
+	},
+	vim.filetype.add({
+		pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+	}),
 }
-
-return options
