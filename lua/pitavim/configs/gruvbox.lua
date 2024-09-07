bc68cfdc2c5c0ffa7eafcb3857c299b80b6374de
pita <itaymalka37@gmail.com>
2024-09-07 20:26:46 +0300

    s

--- lua/pitavim/configs/gruvbox.lua
+++ lua/pitavim/configs/gruvbox.lua
@@ -1,26 +1,26 @@
 require("gruvbox").setup({
-	terminal_colors = true, -- add neovim terminal colors
-	undercurl = true,
-	underline = true,
-	bold = true,
-	italic = {
-		strings = true,
-		emphasis = true,
-		comments = true,
-		operators = false,
-		folds = true,
-	},
-	strikethrough = true,
-	invert_selection = false,
-	invert_signs = false,
-	invert_tabline = false,
-	invert_intend_guides = false,
-	inverse = true, -- invert background for search, diffs, statuslines and errors
-	contrast = "hard", -- can be "hard", "soft" or empty string
-	palette_overrides = {},
-	overrides = {},
-	dim_inactive = false,
-	transparent_mode = false,
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
 })
 vim.o.background = "dark"
 vim.cmd("colorscheme gruvbox")
@@ -29,38 +29,38 @@ vim.cmd.colorscheolors = true
 --neovide function
 
 local function neovide_gruvbox()
-	require("gruvbox").setup({
-		terminal_colors = true,
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true,
-		contrast = "",
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = not vim.g.neovide,
-	})
+  require("gruvbox").setup({
+    terminal_colors = true,
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true,
+    contrast = "",
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = not vim.g.neovide,
+  })
 end
 
 neovide_gruvbox()
 
 if vim.g.neovide then
-	vim.g.neovide_transparency = 1.0
-	vim.g.transparency = 1.0
-	neovide_gruvbox()
-	vim.cmd("colorscheme gruvbox")
-	vim.g.neovide_background_color = "#32302F"
+  vim.g.neovide_transparency = 1.0
+  vim.g.transparency = 1.0
+  neovide_gruvbox()
+  vim.cmd("colorscheme gruvbox")
+  vim.g.neovide_background_color = "#32302F"
 end
