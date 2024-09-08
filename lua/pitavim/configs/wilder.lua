343b28247b964c9c66135e900eba345d322f0a8e
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:57:16 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,35 +1,50 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
 
--- Disable Python remote plugin
+-- Disable Python remote plugin for better performance
 wilder.set_option("use_python_remote_plugin", 0)
 
+-- Enable fuzzy matching
 wilder.set_option("pipeline", {
-  wilder.branch(
-    wilder.cmdline_pipeline({
-      fuzzy = 1,
-      fuzzy_filter = wilder.lua_fzy_filter(),
-    }),
-    wilder.vim_search_pipeline()
-  ),
+	wilder.branch(
+		wilder.cmdline_pipeline({
+			fuzzy = 1,
+			set_pcre2_pattern = 1,
+		}),
+		wilder.vim_search_pipeline()
+	),
+})
+
+-- Enable devicons
+local highlighter = wilder.highlighter_with_gradient({
+	wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+})
+
+-- Configure renderer with borders
+local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+	highlights = {
+		border = "Normal", -- highlight to use for the border
+	},
+	-- 'single', 'double', 'rounded' or 'solid'
+	-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+	border = "rounded",
+	pumblend = 20,
+	left = { " ", wilder.popupmenu_devicons() },
+	right = { " ", wilder.popupmenu_scrollbar() },
+}))
+
+local wildmenu_renderer = wilder.wildmenu_renderer({
+	highlighter = highlighter,
+	separator = " · ",
+	left = { " ", wilder.wildmenu_devicons() },
+	right = { " ", wilder.wildmenu_index() },
 })
 
 wilder.set_option(
-  "renderer",
-  wilder.renderer_mux({
-    [":"] = wilder.popupmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-      left = {
-        " ",
-        wilder.popupmenu_devicons(),
-      },
-      right = {
-        " ",
-        wilder.popupmenu_scrollbar(),
-      },
-    }),
-    ["/"] = wilder.wildmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-    }),
-  })
+	"renderer",
+	wilder.renderer_mux({
+		[":"] = popupmenu_renderer,
+		["/"] = wildmenu_renderer,
+		["?"] = wildmenu_renderer,
+	})
 )
