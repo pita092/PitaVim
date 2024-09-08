423754000e8532f8bea7168311a9f224221c66bc
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:43:58 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,11 +1,14 @@
+local wilder = require("wilder")
 wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-    highlights = {
-      border = "Normal", -- highlight to use for the border
-    },
-    -- 'single', 'double', 'rounded' or 'solid'
-    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-    border = "rounded",
-  }))
+	"renderer",
+	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+		highlights = {
+			border = "Normal", -- highlight to use for the border
+		},
+		-- 'single', 'double', 'rounded' or 'solid'
+		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+		pumblend = 20,
+
+		border = "rounded",
+	}))
 )
