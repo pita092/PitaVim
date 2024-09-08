5c3b3e1ff8232124774d6df2d6ab64ea6727c45a
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 19:52:16 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -116,65 +116,6 @@ cmp.setup({
     { name = "nvim_lua" },
     { name = "treesitter" },
   }),
-  -- formatting = {
-  -- 	fields = { "menu", "abbr", "kind" },
-  -- 	expandable_indicator = true,
-  -- 	format = function(entry, vim_item)
-  -- 		local kind = require("lspkind").cmp_format({ mode = "text", maxwidth = 50 })(entry, vim_item)
-  -- 		local strings = vim.split(kind.kind, " ", { trimempty = true })
-  -- 		vim.api.nvim_set_hl(0, "CmpSel", { bg = "#fbf1c7", fg = "#282828" })
-  --
-  -- 		-- This concatenates the icons with the name of the item kind
-  -- 		-- NOTE: Don't remove the line below if you don't want the CMP to go haywire
-  -- 		kind.menu = "" .. (strings[2] or "") .. ""
-  --
-  -- 		-- Define your icons
-  -- 		local icons = {
-  -- 			Text = "󰉿",
-  -- 			Method = "󰆧",
-  -- 			Function = "󰊕",
-  -- 			Constructor = "",
-  -- 			Field = "󰜢",
-  -- 			Variable = "󰀫",
-  -- 			Class = "󰠱",
-  -- 			Interface = "",
-  -- 			Module = "",
-  -- 			Property = "󰜢",
-  -- 			Unit = "󰑭",
-  -- 			Value = "󰎠",
-  -- 			Enum = "",
-  -- 			Keyword = "󰌋",
-  -- 			Snippet = "",
-  -- 			Color = "󰏘",
-  -- 			File = "󰈙",
-  -- 			Reference = "󰈇",
-  -- 			Folder = "󰉋",
-  -- 			EnumMember = "",
-  -- 			Constant = "󰏿",
-  -- 			Struct = "󰙅",
-  -- 			Event = "",
-  -- 			Operator = "󰆕",
-  -- 			TypeParameter = "",
-  -- 		}
-  --
-  -- 		-- Set the icon and kind
-  -- 		-- kind.kind = string.format('%s %s', icons[vim_item.kind] or '', vim_item.kind)
-  -- 		vim_item.kind = (icons[vim_item.kind] or "") .. ""
-  --
-  -- 		-- Set the source
-  -- 		vim_item.menu = ({
-  -- 			nvim_lsp = "[LSP]",
-  -- 			luasnip = "[Snippet]",
-  -- 			buffer = "[Buffer]",
-  -- 			path = "[Path]",
-  -- 		})[entry.source.name]
-  --
-  -- 		vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#928374", italic = true })
-  --
-  -- 		return kind, vim_item
-  -- 	end,
-  -- },
-
   sorting = {
     comparators = {
       cmp.config.compare.offset,
@@ -190,6 +131,5 @@ cmp.setup({
 })
 vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#fbf1c7", bg = "NONE" })
 vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#fabd2f", bg = "NONE", bold = true })
-vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#c678dd", bg = "NONE" })
 vim.api.nvim_set_hl(0, "Pmenu", { fg = "#fbf1c7", bg = "#282828" })
 vim.api.nvim_set_hl(0, "PmenuSel", { fg = "NONE", bg = "#fbf1c7" })
