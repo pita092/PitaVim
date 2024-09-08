ae5a7ae6f7740666893588eac60c882c01836f51
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 19:36:54 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -214,8 +214,8 @@ cmp.setup({
     },
   },
 })
-vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#abb2bf", bg = "NONE" })
-vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#61afef", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#fbf1c7", bg = "NONE" })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#fabd2f", bg = "NONE", bold = true })
 vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#c678dd", bg = "NONE" })
 vim.api.nvim_set_hl(0, "Pmenu", { fg = "#fbf1c7", bg = "#3c3836" })
 vim.api.nvim_set_hl(0, "PmenuSel", { fg = "NONE", bg = "#fbf1c7" })
