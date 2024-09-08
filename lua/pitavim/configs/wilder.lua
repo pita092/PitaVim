5baf144c66d424cf6bbd0752291e7c5671a1771b
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 08:18:10 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,43 +1,27 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
-wilder.set_option("pipeline", {
-  wilder.branch(
-    wilder.python_file_finder_pipeline({
-      -- to use ripgrep : {'rg', '--files'}
-      -- to use fd      : {'fd', '-tf'}
-      file_command = { "find", ".", "-type", "f", "-printf", "%P\n" },
-      -- to use fd      : {'fd', '-td'}
-      dir_command = { "find", ".", "-type", "d", "-printf", "%P\n" },
-      -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
-      -- found at https://github.com/nixprime/cpsm
-      filters = { "fuzzy_filter", "difflib_sorter" },
-    }),
-    wilder.cmdline_pipeline(),
-    wilder.python_search_pipeline()
-  ),
-})
 wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer({
-    -- highlighter applies highlighting to the candidates
-    highlighter = wilder.basic_highlighter(),
-  })
+	"renderer",
+	wilder.popupmenu_renderer({
+		-- highlighter applies highlighting to the candidates
+		highlighter = wilder.basic_highlighter(),
+	})
 )
 wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer({
-    pumblend = 20,
-  })
+	"renderer",
+	wilder.popupmenu_renderer({
+		pumblend = 20,
+	})
 )
 wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-    highlights = {
-      border = "Normal", -- highlight to use for the border
-    },
-    -- 'single', 'double', 'rounded' or 'solid'
-    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-    border = "rounded",
-  }))
+	"renderer",
+	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+		highlights = {
+			border = "Normal", -- highlight to use for the border
+		},
+		-- 'single', 'double', 'rounded' or 'solid'
+		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+		border = "rounded",
+	}))
 )
 vim.api.nvim_set_hl(0, "WildMenu", { fg = "#fbf1c7", bg = "#282828", italic = true })
