174e1a945aaeb053f7c1d67358934af1e8cebc7f
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 08:20:37 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,34 +1,80 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
--- Disable Python remote plugin
-wilder.set_option("use_python_remote_plugin", 0)
 
 wilder.set_option("pipeline", {
   wilder.branch(
+    wilder.python_file_finder_pipeline({
+      file_command = function(ctx, arg)
+        if string.find(arg, ".") ~= nil then
+          return { "fdfind", "-tf", "-H" }
+        else
+          return { "fdfind", "-tf" }
+        end
+      end,
+      dir_command = { "fd", "-td" },
+      filters = { "cpsm_filter" },
+    }),
+    wilder.substitute_pipeline({
+      pipeline = wilder.python_search_pipeline({
+        skip_cmdtype_check = 1,
+        pattern = wilder.python_fuzzy_pattern({
+          start_at_boundary = 0,
+        }),
+      }),
+    }),
     wilder.cmdline_pipeline({
-      fuzzy = 1,
+      fuzzy = 2,
       fuzzy_filter = wilder.lua_fzy_filter(),
     }),
-    wilder.vim_search_pipeline()
+    {
+      wilder.check(function(ctx, x)
+        return x == ""
+      end),
+      wilder.history(),
+    },
+    wilder.python_search_pipeline({
+      pattern = wilder.python_fuzzy_pattern({
+        start_at_boundary = 0,
+      }),
+    })
   ),
+})
+
+local highlighters = {
+  wilder.pcre2_highlighter(),
+  wilder.lua_fzy_highlighter(),
+}
+
+local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+  border = "rounded",
+  empty_message = wilder.popupmenu_empty_message_with_spinner(),
+  highlighter = highlighters,
+  left = {
+    " ",
+    wilder.popupmenu_devicons(),
+    wilder.popupmenu_buffer_flags({
+      flags = " a + ",
+      icons = { ["+"] = "", a = "", h = "" },
+    }),
+  },
+  right = {
+    " ",
+    wilder.popupmenu_scrollbar(),
+  },
+}))
+
+local wildmenu_renderer = wilder.wildmenu_renderer({
+  highlighter = highlighters,
+  separator = " · ",
+  left = { " ", wilder.wildmenu_spinner(), " " },
+  right = { " ", wilder.wildmenu_index() },
 })
 
 wilder.set_option(
   "renderer",
   wilder.renderer_mux({
-    [":"] = wilder.popupmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-      left = {
-        " ",
-        wilder.popupmenu_devicons(),
-      },
-      right = {
-        " ",
-        wilder.popupmenu_scrollbar(),
-      },
-    }),
-    ["/"] = wilder.wildmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-    }),
+    [":"] = popupmenu_renderer,
+    ["/"] = wildmenu_renderer,
+    substitute = wildmenu_renderer,
   })
 )
