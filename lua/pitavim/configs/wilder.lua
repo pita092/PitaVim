7df42c72699800b56071cd788b626200850d8192
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:38:00 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,24 +1,13 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
+
+wilder.set_option("pipeline", {
+  wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
+})
+
 wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		-- highlighter applies highlighting to the candidates
-		highlighter = {
-			wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
-			wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
-			-- at https://github.com/romgrk/fzy-lua-native
-		},
-		highlights = {
-			accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#b8bb26" } }),
-		},
-		left = { " ", wilder.popupmenu_devicons() },
-		right = { " ", wilder.popupmenu_scrollbar() },
-	})
-)
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		pumblend = 20,
-	})
+  "renderer",
+  wilder.wildmenu_renderer({
+    highlighter = wilder.basic_highlighter(),
+  })
 )
