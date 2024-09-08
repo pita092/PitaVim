9b50b1005d5c19d68e332f4aa5c45c6533ef6b8a
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:54:01 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -32,20 +32,3 @@ wilder.set_option(
 		}),
 	})
 )
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-		highlighter = {
-			wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
-			wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
-			-- at https://github.com/romgrk/fzy-lua-native
-		},
-		highlights = {
-			border = "Normal", -- highlight to use for the border
-			accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#b8bb26" } }),
-		},
-		-- 'single', 'double', 'rounded' or 'solid'
-		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-		border = "rounded",
-	}))
-)
