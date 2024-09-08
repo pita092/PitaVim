f8947de8ed5c119e677e7810128f3c2145be0a90
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 19:55:41 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -1,9 +1,34 @@
 require("nvim-autopairs").setup({})
 
-local luasnip = require("luasnip")
 local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
-
+local kind_icons = {
+	Text = "󰉿",
+	Method = "󰆧",
+	Function = "󰊕",
+	Constructor = "",
+	Field = "󰜢",
+	Variable = "󰀫",
+	Class = "󰠱",
+	Interface = "",
+	Module = "",
+	Property = "󰜢",
+	Unit = "󰑭",
+	Value = "󰎠",
+	Enum = "",
+	Keyword = "󰌋",
+	Snippet = "",
+	Color = "󰏘",
+	File = "󰈙",
+	Reference = "󰈇",
+	Folder = "󰉋",
+	EnumMember = "",
+	Constant = "󰏿",
+	Struct = "󰙅",
+	Event = "",
+	Operator = "󰆕",
+	TypeParameter = "",
+}
 local blankaborder = {
 	border = { "", "", "", "", "", "", "", "" },
 	winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
@@ -22,75 +47,6 @@ require("luasnip").filetype_extend("c", { "cdoc" })
 
 cmp.setup({
 
-	formatting = {
-		fields = { "abbr", "kind" },
-		format = function(entry, vim_item)
-			-- Define your icons
-			local icons = {
-				Text = "󰉿",
-				Method = "󰆧",
-				Function = "󰊕",
-				Constructor = "",
-				Field = "󰜢",
-				Variable = "󰀫",
-				Class = "󰠱",
-				Interface = "",
-				Module = "",
-				Property = "󰜢",
-				Unit = "󰑭",
-				Value = "󰎠",
-				Enum = "",
-				Keyword = "󰌋",
-				Snippet = "",
-				Color = "󰏘",
-				File = "󰈙",
-				Reference = "󰈇",
-				Folder = "󰉋",
-				EnumMember = "",
-				Constant = "󰏿",
-				Struct = "󰙅",
-				Event = "",
-				Operator = "󰆕",
-				TypeParameter = "",
-			}
-
-			-- Set the icon and kind
-			local icon = icons[vim_item.kind] or ""
-			vim_item.kind = string.format("%s %s", icon, vim_item.kind)
-
-			-- Set the abbreviation
-			local abbr = vim_item.abbr
-			local max_abbr_width = 30 -- Adjust this as needed
-			if #abbr > max_abbr_width then
-				vim_item.abbr = vim.fn.strcharpart(abbr, 0, max_abbr_width - 3) .. "..."
-			end
-
-			-- Add a single space after the abbreviation
-			vim_item.abbr = vim_item.abbr .. " "
-
-			-- Set the source (menu)
-			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-			})[entry.source.name] or ""
-
-			-- Trim spaces from the end of the menu entry
-			vim_item.menu = vim_item.menu:gsub("%s+", " ")
-
-			return vim_item
-		end,
-	},
-
-	window = {
-		completion = {
-			border = "single",
-			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
-			col_offset = 0, -- Adjust this to control the horizontal alignment
-			side_padding = 0, -- Remove side padding
-		},
-	},
 	snippet = {
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body)
@@ -116,6 +72,22 @@ cmp.setup({
 		{ name = "nvim_lua" },
 		{ name = "treesitter" },
 	}),
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		expandable_indicator = true,
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, " ", { trimempty = true })
+			kind.kind = string.format("%s  %s", kind_icons[vim_item.kind], strings[1])
+			vim.api.nvim_set_hl(0, "CmpSel", { bg = "#fbf1c7", fg = "#282828" })
+			-- This concatenates the icons with the name of the item kind
+
+			-- NOTE: Don't remove the line below if you don't want the CMP to go haywire
+			kind.menu = "" .. (strings[2] or "") .. ""
+
+			return kind
+		end,
+	},
 	sorting = {
 		comparators = {
 			cmp.config.compare.offset,
