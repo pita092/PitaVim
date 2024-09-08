2e2d732c0ecb7d81283ecc23954ca56c7a8b4e50
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 19:19:31 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end,
+  },
+  {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
     config = function()
---
+++ lua/pitavim/settings.lua
@@ -0,0 +1,12 @@
+local settings = {
+	theme = "neofusion", -- ayu|gruvbox|neofusion
+	indentChar = "│", -- │, |, ¦, ┆, ┊
+	separatorChar = "-", -- ─, -, .
+	aspect = "clean", -- normal|clean
+	lualine_separator = "rect", -- rect|triangle|semitriangle|curve
+	cmp_style = "nvchad", -- default|nvchad
+	cmp_icons_style = "vscode", -- devicons|vscode
+	transparent_mode = true,
+}
+
+return settings
