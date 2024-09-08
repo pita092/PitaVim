6de17c9b071d91cb1c6f6702fc9c6240d93394b7
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 08:20:02 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -1,27 +1,34 @@
 local wilder = require("wilder")
 wilder.setup({ modes = { ":", "/", "?" } })
+-- Disable Python remote plugin
+wilder.set_option("use_python_remote_plugin", 0)
+
+wilder.set_option("pipeline", {
+  wilder.branch(
+    wilder.cmdline_pipeline({
+      fuzzy = 1,
+      fuzzy_filter = wilder.lua_fzy_filter(),
+    }),
+    wilder.vim_search_pipeline()
+  ),
+})
+
 wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		-- highlighter applies highlighting to the candidates
-		highlighter = wilder.basic_highlighter(),
-	})
+  "renderer",
+  wilder.renderer_mux({
+    [":"] = wilder.popupmenu_renderer({
+      highlighter = wilder.lua_fzy_highlighter(),
+      left = {
+        " ",
+        wilder.popupmenu_devicons(),
+      },
+      right = {
+        " ",
+        wilder.popupmenu_scrollbar(),
+      },
+    }),
+    ["/"] = wilder.wildmenu_renderer({
+      highlighter = wilder.lua_fzy_highlighter(),
+    }),
+  })
 )
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer({
-		pumblend = 20,
-	})
-)
-wilder.set_option(
-	"renderer",
-	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-		highlights = {
-			border = "Normal", -- highlight to use for the border
-		},
-		-- 'single', 'double', 'rounded' or 'solid'
-		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-		border = "rounded",
-	}))
-)
-vim.api.nvim_set_hl(0, "WildMenu", { fg = "#fbf1c7", bg = "#282828", italic = true })
