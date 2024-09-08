3d7d41214c43b926bccdce1e94e5c18d18f0e47d
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 17:48:07 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -1,243 +1,127 @@
--- require("nvim-autopairs").setup({})
---
--- local luasnip = require("luasnip")
--- local cmp = require("cmp")
--- local cmp_action = require("lsp-zero").cmp_action()
---
--- local blankaborder = {
--- 	border = { "", "", "", "", "", "", "", "" },
--- 	winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
--- }
---
--- local borderstyle = {
--- 	border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
--- 	winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
--- }
--- local border2 =
--- 	{
--- 		border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
--- 		winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
--- 	}, require("luasnip.loaders.from_vscode").lazy_load()
--- require("luasnip").filetype_extend("c", { "cdoc" })
---
--- cmp.setup({
--- 	snippet = {
--- 		expand = function(args)
--- 			require("luasnip").lsp_expand(args.body)
--- 		end,
--- 	},
--- 	window = {
--- 		completion = borderstyle,
--- 		documentation = borderstyle,
--- 	},
--- 	mapping = cmp.mapping.preset.insert({
--- 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
--- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
--- 		["<C-Space>"] = cmp.mapping.complete(),
--- 		["<Tab>"] = cmp_action.luasnip_supertab(),
--- 		["<C-Tab>"] = cmp_action.luasnip_shift_supertab(),
--- 		["<C-e>"] = cmp.mapping.abort(),
--- 		["<CR>"] = cmp.mapping.confirm({ select = true }),
--- 		["<C-j>"] = cmp.mapping.select_next_item(),
--- 		["<C-k>"] = cmp.mapping.select_prev_item(),
--- 	}),
--- 	sources = cmp.config.sources({
--- 		{ name = "nvim_lsp" },
--- 		{ name = "luasnip" },
--- 		{ name = "buffer" },
--- 		{ name = "path" },
--- 		{ name = "luasnip" },
--- 		{ name = "nvim_lua" },
--- 		{ name = "treesitter" },
--- 	}),
--- 	formatting = {
--- 		fields = { "kind", "abbr", "menu" },
--- 		expandable_indicator = true,
--- 		format = function(entry, vim_item)
--- 			local kind = require("lspkind").cmp_format({ mode = "text", maxwidth = 50 })(entry, vim_item)
--- 			local strings = vim.split(kind.kind, " ", { trimempty = true })
--- 			vim.api.nvim_set_hl(0, "CmpSel", { bg = "#fbf1c7", fg = "#282828" })
---
--- 			-- This concatenates the icons with the name of the item kind
--- 			-- NOTE: Don't remove the line below if you don't want the CMP to go haywire
--- 			kind.menu = "" .. (strings[2] or "") .. ""
---
--- 			-- Define your icons
--- 			local icons = {
--- 				Text = "󰉿",
--- 				Method = "󰆧",
--- 				Function = "󰊕",
--- 				Constructor = "",
--- 				Field = "󰜢",
--- 				Variable = "󰀫",
--- 				Class = "󰠱",
--- 				Interface = "",
--- 				Module = "",
--- 				Property = "󰜢",
--- 				Unit = "󰑭",
--- 				Value = "󰎠",
--- 				Enum = "",
--- 				Keyword = "󰌋",
--- 				Snippet = "",
--- 				Color = "󰏘",
--- 				File = "󰈙",
--- 				Reference = "󰈇",
--- 				Folder = "󰉋",
--- 				EnumMember = "",
--- 				Constant = "󰏿",
--- 				Struct = "󰙅",
--- 				Event = "",
--- 				Operator = "󰆕",
--- 				TypeParameter = "",
--- 			}
---
--- 			-- Set the icon and kind
--- 			-- kind.kind = string.format('%s %s', icons[vim_item.kind] or '', vim_item.kind)
--- 			vim_item.kind = (icons[vim_item.kind] or "") .. ""
---
--- 			-- Set the source
--- 			vim_item.menu = ({
--- 				nvim_lsp = "[LSP]",
--- 				luasnip = "[Snippet]",
--- 				buffer = "[Buffer]",
--- 				path = "[Path]",
--- 			})[entry.source.name]
---
--- 			vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#928374", italic = true })
---
--- 			return kind, vim_item
--- 		end,
--- 	},
--- 	sorting = {
--- 		comparators = {
--- 			cmp.config.compare.offset,
--- 			cmp.config.compare.exact,
--- 			cmp.config.compare.score,
--- 			require("cmp-under-comparator").under,
--- 			cmp.config.compare.kind,
--- 			cmp.config.compare.sort_text,
--- 			cmp.config.compare.length,
--- 			cmp.config.compare.order,
--- 		},
--- 	},
--- })
--- cmp.setup.cmdline("@", { enabled = false })
--- cmp.setup.cmdline(">", { enabled = false })
--- cmp.setup.cmdline("-", { enabled = false })
--- cmp.setup.cmdline("=", { enabled = false })
+require("nvim-autopairs").setup({})
 
+local luasnip = require("luasnip")
 local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
-local cmp_ui = require("nvconfig").ui.cmp
-local cmp_style = cmp_ui.style
-
-local field_arrangement = {
-	atom = { "kind", "abbr", "menu" },
-	atom_colored = { "kind", "abbr", "menu" },
+local blankaborder = {
+	border = { "", "", "", "", "", "", "", "" },
+	winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
 }
 
-local formatting_style = {
-	-- default fields order i.e completion word + item.kind + item.kind icons
-	fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
-
-	format = function(_, item)
-		local icons = require("nvchad.icons.lspkind")
-		local icon = (cmp_ui.icons and icons[item.kind]) or ""
-
-		if cmp_style == "atom" or cmp_style == "atom_colored" then
-			icon = " " .. icon .. " "
-			item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
-			item.kind = icon
-		else
-			icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
-			item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
-		end
-
-		return item
-	end,
+local borderstyle = {
+	border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+	winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
 }
+local border2 =
+	{
+		border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" },
+		winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+	}, require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip").filetype_extend("c", { "cdoc" })
 
-local function border(hl_name)
-	return {
-		{ "╭", hl_name },
-		{ "─", hl_name },
-		{ "╮", hl_name },
-		{ "│", hl_name },
-		{ "╯", hl_name },
-		{ "─", hl_name },
-		{ "╰", hl_name },
-		{ "│", hl_name },
-	}
-end
-
-local options = {
-	completion = {
-		completeopt = "menu,menuone",
-	},
-
-	window = {
-		completion = {
-			side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-			winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-			scrollbar = false,
-		},
-		documentation = {
-			border = border("CmpDocBorder"),
-			winhighlight = "Normal:CmpDoc",
-		},
-	},
+cmp.setup({
 	snippet = {
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
-
-	formatting = formatting_style,
-
-	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	window = {
+		completion = borderstyle,
+		documentation = borderstyle,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		}),
-
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif require("luasnip").expand_or_jumpable() then
-				require("luasnip").expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif require("luasnip").jumpable(-1) then
-				require("luasnip").jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	},
-	sources = {
+		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<C-Tab>"] = cmp_action.luasnip_shift_supertab(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
-		{ name = "nvim_lua" },
 		{ name = "path" },
+		{ name = "luasnip" },
+		{ name = "nvim_lua" },
+		{ name = "treesitter" },
+	}),
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		expandable_indicator = true,
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, " ", { trimempty = true })
+			vim.api.nvim_set_hl(0, "CmpSel", { bg = "#fbf1c7", fg = "#282828" })
+
+			-- This concatenates the icons with the name of the item kind
+			-- NOTE: Don't remove the line below if you don't want the CMP to go haywire
+			kind.menu = "" .. (strings[2] or "") .. ""
+
+			-- Define your icons
+			local icons = {
+				Text = "󰉿",
+				Method = "󰆧",
+				Function = "󰊕",
+				Constructor = "",
+				Field = "󰜢",
+				Variable = "󰀫",
+				Class = "󰠱",
+				Interface = "",
+				Module = "",
+				Property = "󰜢",
+				Unit = "󰑭",
+				Value = "󰎠",
+				Enum = "",
+				Keyword = "󰌋",
+				Snippet = "",
+				Color = "󰏘",
+				File = "󰈙",
+				Reference = "󰈇",
+				Folder = "󰉋",
+				EnumMember = "",
+				Constant = "󰏿",
+				Struct = "󰙅",
+				Event = "",
+				Operator = "󰆕",
+				TypeParameter = "",
+			}
+
+			-- Set the icon and kind
+			-- kind.kind = string.format('%s %s', icons[vim_item.kind] or '', vim_item.kind)
+			vim_item.kind = (icons[vim_item.kind] or "") .. ""
+
+			-- Set the source
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
+			})[entry.source.name]
+
+			vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#928374", italic = true })
+
+			return kind, vim_item
+		end,
 	},
-}
-
-if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
-	options.window.completion.border = border("CmpBorder")
-end
-
-return options
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			require("cmp-under-comparator").under,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
+})
+cmp.setup.cmdline("@", { enabled = false })
+cmp.setup.cmdline(">", { enabled = false })
+cmp.setup.cmdline("-", { enabled = false })
+cmp.setup.cmdline("=", { enabled = false })
