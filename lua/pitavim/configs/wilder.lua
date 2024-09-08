27c7720a005366b64177bab32083d27deb9ed249
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:50:23 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -4,42 +4,47 @@ wilder.setup({ modes = { ":", "/", "?" } })
 wilder.set_option("use_python_remote_plugin", 0)
 
 wilder.set_option("pipeline", {
-  wilder.branch(
-    wilder.cmdline_pipeline({
-      fuzzy = 1,
-      fuzzy_filter = wilder.lua_fzy_filter(),
-    }),
-    wilder.vim_search_pipeline()
-  ),
+	wilder.branch(
+		wilder.cmdline_pipeline({
+			fuzzy = 1,
+			fuzzy_filter = wilder.lua_fzy_filter(),
+		}),
+		wilder.vim_search_pipeline()
+	),
 })
 
 wilder.set_option(
-  "renderer",
-  wilder.renderer_mux({
-    [":"] = wilder.popupmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-      left = {
-        " ",
-        wilder.popupmenu_devicons(),
-      },
-      right = {
-        " ",
-        wilder.popupmenu_scrollbar(),
-      },
-    }),
-    ["/"] = wilder.wildmenu_renderer({
-      highlighter = wilder.lua_fzy_highlighter(),
-    }),
-  })
+	"renderer",
+	wilder.renderer_mux({
+		[":"] = wilder.popupmenu_renderer({
+			highlighter = wilder.lua_fzy_highlighter(),
+			left = {
+				" ",
+				wilder.popupmenu_devicons(),
+			},
+			right = {
+				" ",
+				wilder.popupmenu_scrollbar(),
+			},
+		}),
+		["/"] = wilder.wildmenu_renderer({
+			highlighter = wilder.lua_fzy_highlighter(),
+		}),
+	})
 )
 wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-    highlights = {
-      border = "Normal", -- highlight to use for the border
-    },
-    -- 'single', 'double', 'rounded' or 'solid'
-    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-    border = "rounded",
-  }))
+	"renderer",
+	wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+		highlights = {
+			border = "Normal",
+			wilder.lua_pcre2_highlighter(),
+			wilder.lua_fzy_highlighter(),
+		},
+		highlights = {
+			accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#b8bb26" } }),
+		},
+		-- 'single', 'double', 'rounded' or 'solid'
+		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+		border = "rounded",
+	}))
 )
