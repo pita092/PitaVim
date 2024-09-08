8f8f84535200a538e47485e44b6be6651244e6b6
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 18:21:17 +0300

    s

--- lua/pitavim/scripts/tabline.lua
+++ lua/pitavim/scripts/tabline.lua
@@ -75,57 +75,78 @@
 local M = {}
 
 local function get_file_icon(filename)
-	-- Your existing get_file_icon function remains unchanged
+  local icon = {
+    ["lua"] = "󰢱 ",
+    ["py"] = " ",
+    ["js"] = " ",
+    ["ts"] = " ",
+    ["json"] = " ",
+    ["cpp"] = " ",
+    ["yml"] = "",
+    ["yaml"] = "",
+    ["toml"] = " ",
+    ["md"] = " ",
+    ["txt"] = " ",
+    ["vim"] = " ",
+    ["sh"] = " ",
+    ["bash"] = " ",
+    ["zsh"] = " ",
+    ["fish"] = "  ",
+    ["conf"] = " ",
+    ["ini"] = "",
+    ["gitignore"] = " ",
+    ["gitconfig"] = " ",
+  }
 end
 
 function M.MyTabLabel(n)
-	local buflist = vim.fn.tabpagebuflist(n)
-	local winnr = vim.fn.tabpagewinnr(n)
-	local bufnr = buflist[winnr]
-	local filename = vim.fn.bufname(bufnr)
-	local icon = get_file_icon(filename)
-	local short_name = vim.fn.fnamemodify(filename, ":t")
-	short_name = short_name ~= "" and short_name or "[No Name]"
+  local buflist = vim.fn.tabpagebuflist(n)
+  local winnr = vim.fn.tabpagewinnr(n)
+  local bufnr = buflist[winnr]
+  local filename = vim.fn.bufname(bufnr)
+  local icon = get_file_icon(filename)
+  local short_name = vim.fn.fnamemodify(filename, ":t")
+  short_name = short_name ~= "" and short_name or "[No Name]"
 
-	return {
-		icon = icon,
-		text = short_name,
-	}
+  return {
+    icon = icon,
+    text = short_name,
+  }
 end
 
 function M.MyTabLine()
-	local s = '%#TabLineFill#%{v:lua.require("pitavim.scripts.tabline").ClearHighlight()}'
-	for i = 1, vim.fn.tabpagenr("$") do
-		if i > 1 then
-			s = s .. "|%#TabLineFill#|"
-		end
+  local s = '%#TabLineFill#%{v:lua.require("pitavim.scripts.tabline").ClearHighlight()}'
+  for i = 1, vim.fn.tabpagenr("$") do
+    if i > 1 then
+      s = s .. "|%#TabLineFill#|"
+    end
 
-		if i == vim.fn.tabpagenr() then
-			s = s .. "%#TabLineSel#"
-		else
-			s = s .. "%#TabLine#"
-		end
+    if i == vim.fn.tabpagenr() then
+      s = s .. "%#TabLineSel#"
+    else
+      s = s .. "%#TabLine#"
+    end
 
-		s = s .. "%" .. i .. "T"
+    s = s .. "%" .. i .. "T"
 
-		local label = M.MyTabLabel(i)
-		s = s .. " %#TabLineIcon#" .. label.icon .. "%#TabLine# " .. label.text .. " "
-	end
-	s = s .. "%#TabLineFill#%T"
-	return s
+    local label = M.MyTabLabel(i)
+    s = s .. " %#TabLineIcon#" .. label.icon .. "%#TabLine# " .. label.text .. " "
+  end
+  s = s .. "%#TabLineFill#%T"
+  return s
 end
 
 function M.ClearHighlight()
-	vim.cmd("highlight clear TabLineFill")
-	vim.cmd("highlight clear NeoTreeNormal")
-	vim.cmd("highlight clear NeoTreeNormalNC")
-	vim.cmd('lua vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#fabd2f", bg = "NONE" })')
-	vim.cmd('lua vim.api.nvim_set_hl(0, "TabLineIcon", { fg = "#61afef", bg = "NONE" })')
-	return ""
+  vim.cmd("highlight clear TabLineFill")
+  vim.cmd("highlight clear NeoTreeNormal")
+  vim.cmd("highlight clear NeoTreeNormalNC")
+  vim.cmd('lua vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#fabd2f", bg = "NONE" })')
+  vim.cmd('lua vim.api.nvim_set_hl(0, "TabLineIcon", { fg = "#61afef", bg = "NONE" })')
+  return ""
 end
 
 function M.setup()
-	vim.o.tabline = [[%!v:lua.require'pitavim.scripts.tabline'.MyTabLine()]]
+  vim.o.tabline = [[%!v:lua.require'pitavim.scripts.tabline'.MyTabLine()]]
 end
 
 return M
