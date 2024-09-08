e30e035cd0392e7021716ff542181a0edc31ad6a
pita <154020297+pita092@users.noreply.github.com>
2024-09-08 21:42:40 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -11,20 +11,17 @@ wilder.set_option(
     highlighter = wilder.basic_highlighter(),
   })
 )
-wilder.set_option(
-  "renderer",
-  wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-    pumblend = 20,
-    highlighter = {
-      wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
-      wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
-      -- at https://github.com/romgrk/fzy-lua-native
-    },
-    highlights = {
-      accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
-    },
+wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+  pumblend = 20,
+  highlighter = {
+    wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
+    wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
+    -- at https://github.com/romgrk/fzy-lua-native
+  },
+  highlights = {
+    accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+  },
 
-    -- 'single', 'double', 'rounded' or 'solid'
-    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-  }))
-)
+  -- 'single', 'double', 'rounded' or 'solid'
+  -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+}))
