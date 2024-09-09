f6117daa90c586d1bc1b742d08c16e463308632a
pita <itaymalka37@gmail.com>
2024-09-09 18:48:17 +0300

    s

--- lua/pitavim/configs/lualine.lua
+++ lua/pitavim/configs/lualine.lua
@@ -1,41 +1,77 @@
-require("lualine").setup({
-  options = {
-    icons_enabled = true,
-    theme = "gruvbox",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-      disabled_filetypes = { "NvimTree", "neo-tree" },
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    },
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = { "filename" },
-    lualine_x = { "encoding", "filetype" },
-    lualine_y = { "" },
-    lualine_z = {},
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {},
-})
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local mode = {
+      "mode",
+      fmt = function(str)
+        return " " .. str
+        -- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
+      end,
+    }
+
+    local filename = {
+      "filename",
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 0,        -- 0 = just filename, 1 = relative path, 2 = absolute path
+    }
+
+    local hide_in_width = function()
+      return vim.fn.winwidth(0) > 100
+    end
+
+    local diagnostics = {
+      "diagnostics",
+      sources = { "nvim_diagnostic" },
+      sections = { "error", "warn" },
+      symbols = { error = " ", warn = " ", info = " ", hint = " " },
+      colored = false,
+      update_in_insert = false,
+      always_visible = false,
+      cond = hide_in_width,
+    }
+
+    local diff = {
+      "diff",
+      colored = false,
+      symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+      cond = hide_in_width,
+    }
+
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = "nord", -- Set theme based on environment variable
+        -- Some useful glyphs:
+        -- https://www.nerdfonts.com/cheat-sheet
+        --        
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = { "alpha", "neo-tree" },
+        always_divide_middle = true,
+      },
+      sections = {
+        lualine_a = { mode },
+        lualine_b = { "branch" },
+        lualine_c = { filename },
+        lualine_x = {
+          diagnostics,
+          diff,
+          { "encoding", cond = hide_in_width },
+          { "filetype", cond = hide_in_width },
+        },
+        lualine_y = { "location" },
+        lualine_z = { "progress" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { { "location", padding = 0 } },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { "fugitive" },
+    })
+  end,
+}
