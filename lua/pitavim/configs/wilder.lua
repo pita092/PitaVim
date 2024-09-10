af2fcbd963a8177b189b059c09d50e95ff5191ac
pita <itaymalka37@gmail.com>
2024-09-10 09:19:33 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -22,7 +22,13 @@ vim.api.nvim_set_hl(0, "WilderPopupNormal", { fg = "#fbf1c7", bg = "NONE" })
 
 -- Configure renderer with borders and custom highlights
 local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+	highlighter = {
+		wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
+		wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
+		-- at https://github.com/romgrk/fzy-lua-native
+	},
 	highlights = {
+		accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
 		border = "WilderPopupBorder", -- use our custom border highlight
 		default = "WilderPopupNormal", -- use our custom normal highlight
 		selected = "WilderPopupSelected", -- use our custom selected highlight
