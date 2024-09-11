5221d1b5449d81f7eac31538bb43fc5cd26469e5
pita <itaymalka37@gmail.com>
2024-09-11 09:03:57 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -47,3 +47,15 @@ local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_the
   prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
   reverse = 0,            -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
 }))
+
+wilder.set_option(
+  "renderer",
+  wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+    highlights = {
+      border = "Normal", -- highlight to use for the border
+    },
+    -- 'single', 'double', 'rounded' or 'solid'
+    -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+    border = "rounded",
+  }))
+)
