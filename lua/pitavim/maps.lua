5132dd4c7fd8b10e4510d60ceef19a31c88ed625
pita <154020297+pita092@users.noreply.github.com>
2024-09-11 20:28:14 +0300

    s

--- lua/pitavim/maps.lua
+++ lua/pitavim/maps.lua
@@ -71,7 +71,7 @@ map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- new terminals
 map("n", "<leader>v", function()
-  require("nvterm.terminal").toggle("float")
+  require("nvterm.terminal").new("vertical")
 end, { desc = "terminal new vertical window" })
 
 vim.api.nvim_set_keymap("n", "<C-q>", ":bdelete!<CR>", { noremap = true, silent = true })
