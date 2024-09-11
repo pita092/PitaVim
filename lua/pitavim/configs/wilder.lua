4f29b325ef411ab1e04f7e0c4f3da70cba199862
pita <itaymalka37@gmail.com>
2024-09-11 08:58:32 +0300

    s

--- lua/pitavim/configs/wilder.lua
+++ lua/pitavim/configs/wilder.lua
@@ -72,31 +72,27 @@ wilder.setup({ modes = { ":", "/", "?" } })
 wilder.set_option("use_python_remote_plugin", 0)
 
 wilder.set_option("pipeline", {
-	wilder.branch(
-		wilder.cmdline_pipeline({
-			fuzzy = 1,
-			fuzzy_filter = wilder.lua_fzy_filter(),
-		}),
-		wilder.vim_search_pipeline()
-	),
+  wilder.branch(
+    wilder.cmdline_pipeline({
+      fuzzy = 1,
+      fuzzy_filter = wilder.lua_fzy_filter(),
+    }),
+    wilder.vim_search_pipeline()
+  ),
 })
 
 wilder.set_option(
-	"renderer",
-	wilder.renderer_mux({
-		[":"] = wilder.popupmenu_renderer({
-			highlighter = wilder.lua_fzy_highlighter(),
-			left = {
-				" ",
-				wilder.popupmenu_devicons(),
-			},
-			right = {
-				" ",
-				wilder.popupmenu_scrollbar(),
-			},
-		}),
-		["/"] = wilder.wildmenu_renderer({
-			highlighter = wilder.lua_fzy_highlighter(),
-		}),
-	})
+  "renderer",
+  wilder.renderer_mux({
+    [":"] = wilder.popupmenu_renderer({
+      highlighter = wilder.lua_fzy_highlighter(),
+      left = {
+        " ",
+        wilder.popupmenu_devicons(),
+      },
+    }),
+    ["/"] = wilder.wildmenu_renderer({
+      highlighter = wilder.lua_fzy_highlighter(),
+    }),
+  })
 )
