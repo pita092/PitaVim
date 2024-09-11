0ceac9a90e993b6cc2a9004b644c4dd94ed2206b
pita <154020297+pita092@users.noreply.github.com>
2024-09-11 20:53:43 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -47,7 +47,7 @@ require("telescope").setup({
     },
     previewer = false,
     prompt_prefix = "  ",
-    selection_caret = "󰜴 ",
+    selection_caret = "󰜴  ",
     entry_prefix = "  ",
     initial_mode = "insert",
     selection_strategy = "reset",
