dadf0ad9f8d784ac14ae7c29fc8a7a57787dc7c6
pita <itaymalka37@gmail.com>
2024-09-12 10:43:07 +0300

    s

--- lua/pitavim/cmds.lua
+++ lua/pitavim/cmds.lua
@@ -1,54 +1,54 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "UIEnter", "ColorScheme" }, {
-	callback = function()
-		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-		if not normal.bg then
-			return
-		end
-		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-	end,
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then
+      return
+    end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
 })
 
 vim.api.nvim_create_autocmd("UILeave", {
-	callback = function()
-		io.write("\027]111\027\\")
-	end,
+  callback = function()
+    io.write("\027]111\027\\")
+  end,
 })
 
 autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
-	group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
-	callback = function(args)
-		local file = vim.api.nvim_buf_get_name(args.buf)
-		local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+  group = vim.api.nvim_create_augroup("NvFilePost", { clear = true }),
+  callback = function(args)
+    local file = vim.api.nvim_buf_get_name(args.buf)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
 
-		if not vim.g.ui_entered and args.event == "UIEnter" then
-			vim.g.ui_entered = true
-		end
+    if not vim.g.ui_entered and args.event == "UIEnter" then
+      vim.g.ui_entered = true
+    end
 
-		if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
-			vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
-			vim.api.nvim_del_augroup_by_name("NvFilePost")
+    if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+      vim.api.nvim_del_augroup_by_name("NvFilePost")
 
-			vim.schedule(function()
-				vim.api.nvim_exec_autocmds("FileType", {})
+      vim.schedule(function()
+        vim.api.nvim_exec_autocmds("FileType", {})
 
-				if vim.g.editorconfig then
-					require("editorconfig").config(args.buf)
-				end
-			end)
-		end
-	end,
+        if vim.g.editorconfig then
+          require("editorconfig").config(args.buf)
+        end
+      end)
+    end
+  end,
 })
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "neo-tree",
-	callback = function()
-		vim.opt_local.fillchars:append({ eob = " " })
-	end,
+  pattern = "neo-tree",
+  callback = function()
+    vim.opt_local.fillchars:append({ eob = " " })
+  end,
 })
 
 vim.defer_fn(function()
-	vim.cmd([[
+  vim.cmd([[
     highlight SignColumn guibg=NONE ctermbg=NONE
   ]])
 end, 100)
@@ -65,12 +65,13 @@ vim.cmd([[
   highlight  TelescopePromptPrefix guifg=#b8bb26
   highlight TelescopeMatching guifg=#fabd2f
   highlight Pmenu guibg=#3c3836
+  highlight 	TelescopePromptTitle guibg=#fbf1c7
   ]])
 
 ---custom commands
 
 vim.api.nvim_create_user_command("Updt", "Lazy update nvim-plugins", {
-	desc = "Update neovim plugins",
+  desc = "Update neovim plugins",
 })
 vim.cmd([[
   function! LuaAbbreviation()
