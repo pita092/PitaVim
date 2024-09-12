c0de8ec972df8555d249c9a50f618a58816806b9
pita <itaymalka37@gmail.com>
2024-09-12 11:59:20 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,5 +1,24 @@
 return {
   {
+    "princejoogie/chafa.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "m00qek/baleia.nvim",
+    },
+    config = function()
+      require("chafa").setup({
+        render = {
+          min_padding = 5,
+          show_label = true,
+        },
+        events = {
+          update_on_nvim_resize = true,
+        },
+      })
+    end,
+  },
+
+  {
     "goolord/alpha-nvim",
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
