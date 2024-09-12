33bac30534398db6ab6da6c372c9c866f30bebf1
pita <itaymalka37@gmail.com>
2024-09-12 12:01:06 +0300

    s

--- lua/pitavim/configs/alpha.lua
+++ lua/pitavim/configs/alpha.lua
@@ -6,9 +6,9 @@ local chafa = require("chafa")
 
 -- Function to display image using chafa
 local function display_image()
-  local image_path = "/path/to/your/image.png" -- Replace with your image path
+  local image_path = "/mnt/c/users/itaym/Downloads/9740-3942146447(1).png" -- Replace with your image path
   local width = vim.o.columns
-  local height = 20                           -- Adjust as needed
+  local height = 20                                                       -- Adjust as needed
 
   return {
     type = "terminal",
