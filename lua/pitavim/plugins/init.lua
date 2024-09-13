facde58d49373f90dabc04d42111f357d75b4fbb
pita <154020297+pita092@users.noreply.github.com>
2024-09-13 13:27:36 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
 		"goolord/alpha-nvim",
 		lazy = false,
 		-- dependencies = { 'echasnovski/mini.icons' },
