c4af0e5383a9765c531b8da124c6749af14b0258
pita <154020297+pita092@users.noreply.github.com>
2024-09-13 11:59:40 +0300

    s

---
+++ lua/pitavim/configs/alpha.lua
@@ -0,0 +1,284 @@
+-- local header = {
+--   [[                                       .::.                                  ]],
+--   [[                                     .;:**'            AMC                   ]],
+--   [[                                     `                  0                    ]],
+--   [[         .:XHHHHk.              db.   .;;.     dH  MX   0                    ]],
+--   [[       oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN             ]],
+--   [[       QMMMMMb  "MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM        ]],
+--   [[         `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP        ]],
+--   [[          'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM         ]],
+--   [[           ~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP         ]],
+--   [[            ?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM          ]],
+--   [[             MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP          ]],
+--   [[             'MMM.                                             IMX           ]],
+--   [[              ~M!M                                             IMP           ]],
+--   [[                                                                             ]],
+--   [[                                                                             ]],
+--   [[                    ."-,.__                                                  ]],
+--   [[                    `.     `.  ,                                             ]],
+--   [[                 .--'  .._,'"-' `.                                           ]],
+--   [[                .    .'         `'                                           ]],
+--   [[                `.   /          ,'                                           ]],
+--   [[                  `  '--.   ,-"'                                             ]],
+--   [[                   `"`   |  \                                                ]],
+--   [[                      -. \, |                                                ]],
+--   [[                       `--Y.'      ___.                                      ]],
+--   [[                            \     L._, \                                     ]],
+--   [[                  _.,        `.   <  <\                _                     ]],
+--   [[                ,' '           `, `.   | \            ( `                    ]],
+--   [[             ../, `.            `  |    .\`.           \ \_                  ]],
+--   [[            ,' ,..  .           _.,'    ||\l            )  '".               ]],
+--   [[           , ,'   \           ,'.-.`-._,'  |           .  _._`.              ]],
+--   [[         ,' /      \ \        `' ' `--/   | \          / /   ..\             ]],
+--   [[       .'  /        \ .         |\__ - _ ,'` `        / /     `.`.           ]],
+--   [[       |  '          ..         `-...-"  |  `-'      / /        . `.         ]],
+--   [[       | /           |L__           |    |          / /          `. `.       ]],
+--   [[      , /            .   .          |    |         / /             ` `       ]],
+--   [[     / /          ,. ,`._ `-_       |    |  _   ,-' /               ` \      ]],
+--   [[    / .           \"`_/. `-_ \_,.  ,'    +-' `-'  _,        ..,-.    \`.     ]],
+--   [[   .  '         .-f    ,'   `    '.       \__.---'     _   .'   '     \ \    ]],
+--   [[   ' /          `.'    l     .' /          \..      ,_|/   `.  ,'`     L`    ]],
+--   [[   |'      _.-""` `.    \ _,'  `            \ `.___`.'"`-.  , |   |    | \   ]],
+--   [[   ||    ,'      `. `.   '       _,...._        `  |    `/ '  |   '     .|   ]],
+--   [[   ||  ,'          `. ;.,.---' ,'       `.   `.. `-'  .-' /_ .'    ;_   ||   ]],
+--   [[   || '              V      / /           `   | `   ,'   ,' '.    !  `. ||   ]],
+--   [[   ||/            _,-------7 '              . |  `-'    l         /    `||   ]],
+--   [[   . |          ,' .-   ,' ||               | .-.        `.      .'     ||   ]],
+--   [[    `'        ,'    `".'    |               |    `.        '. -.'       `'   ]],
+--   [[             /      ,'      |               |,'    \-.._,.'/'                ]],
+--   [[             .     /        .               .       \    .''                 ]],
+--   [[           .`.    |         `.             /         :_,'.'                  ]],
+--   [[             \ `...\   _     ,'-.        .'         /_.-'                    ]],
+--   [[              `-.__ `,  `'   .  _.>----''.  _  __  /                         ]],
+--   [[                   .'        /"'          |  "'   '_                         ]],
+--   [[                  /_|.-'\ ,".             '.'`__'-( \                        ]],
+--   [[                    / ,"'"\,'               `/  `-.|"                        ]],
+--   [[ ]]
+-- }
+
+-- local header = {
+--   [[                                   .::.                               ]],
+--   [[                                 .;:**'            AMC                ]],
+--   [[                                 `                  0                 ]],
+--   [[     .:XHHHHk.              db.   .;;.     dH  MX   0                 ]],
+--   [[   oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN          ]],
+--   [[   QMMMMMb  "MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM     ]],
+--   [[     `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP     ]],
+--   [[      'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM      ]],
+--   [[       ~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP      ]],
+--   [[        ?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM       ]],
+--   [[         MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP       ]],
+--   [[         'MMM.                                             IMX        ]],
+--   [[          ~M!M                                             IMP        ]],
+--   [[                                                                      ]],
+--   [[                                                                      ]],
+--   [[                                                   /                  ]],
+--   [[                              _,.------....___,.' ',.-.               ]],
+--   [[                           ,-'          _,.--"        |               ]],
+--   [[                         ,'         _.-'              .               ]],
+--   [[                        /   ,     ,'                   `              ]],
+--   [[                       .   /     /                     ``.            ]],
+--   [[                       |  |     .                       \.\           ]],
+--   [[             ____      |___._.  |       __               \ `.         ]],
+--   [[           .'    `---""       ``"-.--"'`  \               .  \        ]],
+--   [[          .  ,            __               `              |   .       ]],
+--   [[          `,'         ,-"'  .               \             |    L      ]],
+--   [[         ,'          '    _.'                -._          /    |      ]],
+--   [[        ,`-.    ,".   `--'                      >.      ,'     |      ]],
+--   [[       . .'\'   `-'       __    ,  ,-.         /  `.__.-      ,'      ]],
+--   [[       ||:, .           ,'  ;  /  / \ `        `.    .      .'/       ]],
+--   [[       j|:D  \          `--'  ' ,'_  . .         `.__, \   , /        ]],
+--   [[      / L:_  |                 .  "' :_;                `.'.'         ]],
+--   [[      .    ""'                  """""'                    V           ]],
+--   [[       `.                                 .    `.   _,..  `           ]],
+--   [[         `,_   .    .                _,-'/    .. `,'   __  `          ]],
+--   [[          ) \`._        ___....----"'  ,'   .'  \ |   '  \  .         ]],
+--   [[         /   `. "`-.--"'         _,' ,'     `---' |    `./  |         ]],
+--   [[        .   _  `""'--.._____..--"   ,             '         |         ]],
+--   [[        | ." `. `-.                /-.           /          ,         ]],
+--   [[        | `._.'    `,_            ;  /         ,'          .          ]],
+--   [[       .'          /| `-.        . ,'         ,           ,           ]],
+--   [[       '-.__ __ _,','    '`-..___;-...__   ,.'\ ____.___.'            ]],
+--   [[       `"^--'..'   '-`-^-'"--    `-^-'`.''"""""`.,^.`.--'             ]],
+--   [[ ]]
+-- }
+
+local header = {
+  [[                                                                              ]],
+  [[                                    ██████                                    ]],
+  [[                                ████▒▒▒▒▒▒████                                ]],
+  [[                              ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                              ]],
+  [[                            ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                            ]],
+  [[                          ██▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒                              ]],
+  [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▓▓▓▓                          ]],
+  [[                          ██▒▒▒▒▒▒  ▒▒▓▓▒▒▒▒▒▒  ▒▒▓▓                          ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██                        ]],
+  [[                        ██▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒██                        ]],
+  [[                        ████  ██▒▒██  ██▒▒▒▒██  ██▒▒██                        ]],
+  [[                        ██      ██      ████      ████                        ]],
+  [[                                                                              ]],
+  [[                                                                              ]],
+}
+
+local make_header = function()
+  local lines = {}
+  for i, line_chars in pairs(header) do
+    local hi = i > 15 and "Bulbasaur" .. (i - 15) or "PokemonLogo" .. i
+    local line = {
+      type = "text",
+      val = line_chars,
+      opts = {
+        hl = "AlphaSpecialKey" .. i,
+        shrink_margin = false,
+        position = "center",
+      },
+    }
+    table.insert(lines, line)
+  end
+
+  local output = {
+    type = "group",
+    val = lines,
+    opts = { position = "center" },
+  }
+
+  return output
+end
+
+local margin_fix = vim.fn.floor(vim.fn.winwidth(0) / 2 - 46 / 2)
+
+local button = function(sc, txt, keybind, padding)
+  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+  local text = padding and (" "):rep(padding) .. txt or txt
+
+  local offset = padding and padding + 3 or 3
+
+  local opts = {
+    width = 46,
+    shortcut = sc,
+    cursor = -1,
+    align_shortcut = "right",
+    hl_shortcut = "AlphaButtonShortcut",
+    hl = {
+      { "AlphaButtonIcon", 0, margin_fix + offset },
+      {
+        "AlphaButton",
+        offset,
+        #text,
+      },
+    },
+  }
+
+  if keybind then
+    opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
+  end
+
+  return {
+    type = "button",
+    val = text,
+    on_press = function()
+      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+      vim.api.nvim_feedkeys(key, "normal", false)
+    end,
+    opts = opts,
+  }
+end
+
+local thingy = io.popen('echo "$(date +%a) $(date +%d) $(date +%b)" | tr -d "\n"')
+local date = thingy:read("*a")
+thingy:close()
+
+local heading = {
+  type = "text",
+  val = "· Today is " .. date .. " ·",
+  opts = {
+    position = "center",
+    hl = "Folded",
+  },
+}
+
+local alpha = require("alpha")
+require("alpha.term")
+
+local terminal = {
+  type = "terminal",
+  command = vim.fn.expand("$HOME") .. "/.config/nvim/lua/lazy/thisisfine.sh",
+  width = 46,
+  height = 25,
+  opts = {
+    redraw = true,
+    window_config = {},
+  },
+}
+
+local section = {
+  header = make_header(),
+  heading = heading,
+  terminal = terminal,
+  buttons = {
+    type = "group",
+    val = {
+      button("p", "ﱮ  Restore Session", ":WorkspacesOpen<CR>"),
+      button("f", "  Recent Files", ":Telescope oldfiles<CR>"),
+      button("u", "  Update Plugins", ":Lazy update<CR>"),
+      button("q", "  Quit", ":qa<CR>"),
+    },
+    opts = {
+      spacing = 1,
+    },
+  },
+}
+
+local marginTopPercent = 0.225
+local headerPadding = vim.fn.max({ 4, vim.fn.floor(vim.fn.winheight(0) * marginTopPercent) })
+
+local padding = function(value)
+  return { type = "padding", val = value }
+end
+
+local config = {
+  layout = {
+    padding(headerPadding),
+    section.terminal,
+    padding(4),
+    section.heading,
+    padding(2),
+    section.buttons,
+  },
+  opts = {
+    margin = margin_fix,
+  },
+}
+
+alpha.setup(config)
+
+-- vim.api.nvim_create_autocmd(
+--   "WinResized",
+--   {
+--     group = "alpha",
+--     callback = function()
+--       alpha.redraw()
+--     end
+--   }
+-- )
+
+-- disable statusline in dashboard
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "alpha",
+  callback = function()
+    local old_laststatus = vim.opt.laststatus
+
+    vim.api.nvim_create_autocmd("BufUnload", {
+      buffer = 0,
+      callback = function()
+        -- vim.opt.laststatus = old_laststatus
+      end,
+    })
+
+    vim.opt.laststatus = 0
+  end,
+})
