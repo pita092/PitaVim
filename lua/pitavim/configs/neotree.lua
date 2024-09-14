5c6f232feb7440b01800fa6eb3fa6930a1882782
pita <154020297+pita092@users.noreply.github.com>
2024-09-14 14:09:21 +0300

    changed neo tree and added tabs to it

--- lua/pitavim/configs/neotree.lua
+++ lua/pitavim/configs/neotree.lua
@@ -9,6 +9,11 @@ require("neo-tree").setup({
     statusline = false,
     content_layout = "center",
     tabs_layout = "equal",
+    sources = {
+      { source = "filesystem", display_name = "Files" },
+      { source = "buffers",    display_name = "Buffers" },
+      { source = "git_status", display_name = "Git" },
+    },
   },
   default_component_configs = {
     -- Your existing default_component_configs here
@@ -45,7 +50,10 @@ require("neo-tree").setup({
   },
 })
 
--- Key mappings for quick source switching
-vim.keymap.set("n", "<leader>e", ":Neotree filesystem<CR>", { desc = "Explorer (filesystem)" })
-vim.keymap.set("n", "<leader>b", ":Neotree buffers<CR>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>g", ":Neotree git_status<CR>", { desc = "Git status" })
+-- Add these key mappings outside of the setup function
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem<CR>", { silent = true })
+vim.keymap.set("n", "<leader>b", ":Neotree buffers<CR>", { silent = true })
+vim.keymap.set("n", "<leader>g", ":Neotree git_status<CR>", { silent = true })
+
+-- Your existing command to reveal Neotree
+vim.cmd([[nnoremap \ :Neotree reveal]])
