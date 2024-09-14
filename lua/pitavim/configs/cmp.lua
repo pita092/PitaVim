ccc51c1d531599b6bc1fb0c64e6c09e1be9575a9
pita <154020297+pita092@users.noreply.github.com>
2024-09-14 16:31:11 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -181,3 +181,8 @@ cmp.setup.cmdline(":", {
 vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#fbf1c7", bg = "NONE" })
 vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#b8bb26", bg = "NONE", bold = true })
 vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#3c3836" })
+
+-- Add these lines for command line completion
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#b8bb26", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#fbf1c7", bg = "NONE" })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#fbf1c7", bg = "NONE" })
