ad1bc56006815ccca9c635b00414f89d755a314b
pita <154020297+pita092@users.noreply.github.com>
2024-09-16 15:15:45 +0300

    s

--- lua/pitavim/highlights.lua
+++ lua/pitavim/highlights.lua
@@ -1,12 +1,12 @@
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeFloatNormal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatNormal", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "#3c3836" })
+vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "#3c3836" })
 
 -- vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#262626", bg = "#3c3836" })
 -- vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = "#fbf1c7", bg = "#3c3836" })
@@ -22,44 +22,44 @@ vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "NONE" })
 -- vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = "#282828", bg = "#fbf1c7" })
 
 local colors = {
-	bg0 = "#282828",
-	bg1 = "#3c3836",
-	bg2 = "#282828",
-	fg1 = "#fbf1c7",
-	fg4 = "#a89984",
-	gray = "#928374",
-	blue = "#458588",
-	aqua = "#689d6a",
-	purple = "#b16286",
-	orange = "#d65d0e",
-	red = "#cc241d",
-	green = "#98971a",
-	yellow = "#d79921",
+  bg0 = "#282828",
+  bg1 = "#3c3836",
+  bg2 = "#282828",
+  fg1 = "#fbf1c7",
+  fg4 = "#a89984",
+  gray = "#928374",
+  blue = "#458588",
+  aqua = "#689d6a",
+  purple = "#b16286",
+  orange = "#d65d0e",
+  red = "#cc241d",
+  green = "#98971a",
+  yellow = "#d79921",
 }
 
 -- Telescope highlight groups
 local highlights = {
-	TelescopeBorder = { fg = colors.bg1, bg = colors.bg1 },
-	TelescopePromptBorder = { fg = colors.bg2, bg = colors.bg2 },
-	TelescopePromptNormal = { fg = colors.fg1, bg = colors.gray },
-	TelescopePromptPrefix = { fg = colors.orange, bg = colors.bg2 },
-	TelescopePromptTitle = { fg = colors.bg0, bg = colors.fg1 },
-	TelescopePreviewTitle = { fg = colors.fg1, bg = colors.green },
-	TelescopeResultsTitle = { fg = colors.bg0, bg = colors.gray },
-	TelescopeSelection = { fg = colors.fg1, bg = colors.bg2 },
-	TelescopeSelectionCaret = { fg = colors.orange, bg = colors.bg2 },
-	TelescopeResultsDiffAdd = { fg = colors.green },
-	TelescopeResultsDiffChange = { fg = colors.yellow },
-	TelescopeResultsDiffDelete = { fg = colors.red },
+  TelescopeBorder = { fg = colors.bg1, bg = colors.bg1 },
+  TelescopePromptBorder = { fg = colors.bg2, bg = colors.bg2 },
+  TelescopePromptNormal = { fg = colors.fg1, bg = colors.gray },
+  TelescopePromptPrefix = { fg = colors.orange, bg = colors.bg2 },
+  TelescopePromptTitle = { fg = colors.bg0, bg = colors.fg1 },
+  TelescopePreviewTitle = { fg = colors.fg1, bg = colors.green },
+  TelescopeResultsTitle = { fg = colors.bg0, bg = colors.gray },
+  TelescopeSelection = { fg = colors.fg1, bg = colors.bg2 },
+  TelescopeSelectionCaret = { fg = colors.orange, bg = colors.bg2 },
+  TelescopeResultsDiffAdd = { fg = colors.green },
+  TelescopeResultsDiffChange = { fg = colors.yellow },
+  TelescopeResultsDiffDelete = { fg = colors.red },
 }
 
 -- Apply highlights
 for hl_group, col in pairs(highlights) do
-	vim.api.nvim_set_hl(0, hl_group, col)
+  vim.api.nvim_set_hl(0, hl_group, col)
 end
 
 --winbar
 
-vim.api.nvim_set_hl(0, "DropBarMenuCurrentContext", { fg = "#fbf1c7", bg = "NONE", italic = true })
+vim.api.nvim_set_hl(0, "DropBarMenuCurrentContext", { fg = "#fbf1c7", bg = "3c3836", italic = true })
 
 --wildmenu
