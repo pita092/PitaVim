9fecf9aef86038dd413eb5f381161bd8f03cf7d1
pita <154020297+pita092@users.noreply.github.com>
2024-09-17 20:58:44 +0300

    s

--- lua/pitavim/configs/linting.lua
+++ lua/pitavim/configs/linting.lua
@@ -6,6 +6,11 @@ lint.linters_by_ft = {
 }
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+require("lint").linters.luacheck.args = {
+	"--globals",
+	"vim",
+}
+
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 	group = lint_augroup,
 	callback = function()
