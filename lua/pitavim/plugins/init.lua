620af1bcd9aa9836a24779bb1a03b9a45d7a4294
pita <154020297+pita092@users.noreply.github.com>
2024-09-17 19:43:03 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -2,6 +2,28 @@ return {
 	{
 		"Bekaboo/deadcolumn.nvim",
 		event = "User FilePost",
+		opts = {
+			scope = "line", ---@type string|fun(): integer
+			---@type string[]|fun(mode: string): boolean
+			modes = function(mode)
+				return mode:find("^[ictRss\x13]") ~= nil
+			end,
+			blending = {
+				threshold = 0.75,
+				colorcode = "#000000",
+				hlgroup = { "Normal", "bg" },
+			},
+			warning = {
+				alpha = 0.4,
+				offset = 0,
+				colorcode = "#FF0000",
+				hlgroup = { "Error", "bg" },
+			},
+			extra = {
+				---@type string?
+				follow_tw = nil,
+			},
+		},
 	},
 	{
 		"jbyuki/venn.nvim",
