074565710ca33937ee56ec80edf1e20b1dbbd092
pita <154020297+pita092@users.noreply.github.com>
2024-09-17 18:30:46 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -342,5 +342,8 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     keys = { "<space>m", "<space>j", "<space>s" },
     dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+    config = function()
+      require("treesj").setup()
+    end,
   },
 }
