454e2a23b022cea153bab8ba264e224d09c80547
pita <154020297+pita092@users.noreply.github.com>
2024-09-17 18:35:21 +0300

    s

--- lua/pitavim/maps.lua
+++ lua/pitavim/maps.lua
@@ -178,3 +178,27 @@ end
 
 -- Set up a keymap to open the picker
 vim.api.nvim_set_keymap("n", "<leader>lp", "<cmd>lua language_picker()<CR>", { noremap = true, silent = true })
+
+--harpoon
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>f", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>1", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "<leader>2", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "<leader>3", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<leader>4", function()
+  ui.nav_file(4)
+end)
+vim.keymap.set("n", "<leader>5", function()
+  ui.nav_file(5)
+end)
