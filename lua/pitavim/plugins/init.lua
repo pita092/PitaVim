2cb48f13a5a8d96b50808500a21f2f80e75ac1c8
pita <154020297+pita092@users.noreply.github.com>
2024-09-17 19:31:36 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -77,11 +77,16 @@ return {
 	-- 	},
 	-- },
 	{
-		"fgheng/winbar.nvim",
-		event = "BufReadPost",
-		config = function()
-			require("winbar").setup()
-		end,
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			-- configurations go here
+		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
