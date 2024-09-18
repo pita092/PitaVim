192139c17610d080e87a75536ec23f39c9891fcf
pita <154020297+pita092@users.noreply.github.com>
2024-09-18 16:55:26 +0300

    s

--- lua/pitavim/configs/lsp.lua
+++ lua/pitavim/configs/lsp.lua
@@ -4,10 +4,10 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.diagnostic.config({
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = "✘",
-			[vim.diagnostic.severity.WARN] = "▲",
-			[vim.diagnostic.severity.HINT] = "⚑",
-			[vim.diagnostic.severity.INFO] = "»",
+			[vim.diagnostic.severity.ERROR] = "E",
+			[vim.diagnostic.severity.WARN] = "W",
+			[vim.diagnostic.severity.HINT] = "H",
+			[vim.diagnostic.severity.INFO] = "I",
 		},
 	},
 })
