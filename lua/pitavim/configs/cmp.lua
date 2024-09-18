e4c0af6e5665787bcb02c90c4207dc851b99dc8d
pita <154020297+pita092@users.noreply.github.com>
2024-09-18 20:02:09 +0300

    s

--- lua/pitavim/configs/cmp.lua
+++ lua/pitavim/configs/cmp.lua
@@ -97,7 +97,7 @@ cmp.setup({
 			-- NOTE: Don't remove the line below if you don't want the CMP to go haywire
 			kind.menu = "" .. (strings[2] or "") .. ""
 
-			return kind, vim_item
+			return kind, menu
 		end,
 	},
 
