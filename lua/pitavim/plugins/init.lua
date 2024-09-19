696222730c11779ebfc1a7f8badd5efc9f5f93c7
pita <itaymalka37@gmail.com>
2024-09-19 10:13:15 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -256,19 +256,6 @@ return {
 		end,
 	},
 	{
-		"maxmx03/solarized.nvim",
-		lazy = false,
-		priority = 1000,
-		---@type solarized.config
-		opts = {},
-		config = function(_, opts)
-			vim.o.termguicolors = true
-			vim.o.background = "dark"
-			require("solarized").setup(opts)
-			vim.cmd.colorscheme("solarized")
-		end,
-	},
-	{
 		"NvChad/nvim-colorizer.lua",
 		event = "VeryLazy",
 		config = function()
