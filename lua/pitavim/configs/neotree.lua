1a3a5b838e71035e7e8a184d16be2d847510cf93
pita <154020297+pita092@users.noreply.github.com>
2024-09-22 16:51:07 +0300

    s

--- lua/pitavim/configs/neotree.lua
+++ lua/pitavim/configs/neotree.lua
@@ -42,7 +42,7 @@ require("neo-tree").setup({
   }),
 
 		source_selector = {
-			winbar = tree,
+			winbar = true,
 			statusline = true,
 		},
 		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
