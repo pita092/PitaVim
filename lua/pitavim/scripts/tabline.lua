2b45b4956621d1d5d7a771ac1f28b4bf1588c34e
pita <itaymalka37@gmail.com>
2024-09-22 12:17:59 +0300

    s

--- lua/pitavim/scripts/tabline.lua
+++ lua/pitavim/scripts/tabline.lua
@@ -403,9 +403,6 @@ function M.ClearHighlight()
   vim.api.nvim_set_hl(0, "TabLineBorder", { fg = "NONE", bg = "NONE" })
   vim.api.nvim_set_hl(0, "TabLineSelBorder", { fg = "#fabd2f", bg = "NONE" })
   vim.api.nvim_set_hl(0, "WarningMsg", { fg = "#fabd2f", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "ErrorMsg", { fg = "#fb4934", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "InfoMsg", { fg = "#83a598", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "HintMsg", { fg = "#8ec07c", bg = "NONE" })
   return ""
 end
 
