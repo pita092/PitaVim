f620fe7820e9571c1dd9267de84abd84d8908611
pita <154020297+pita092@users.noreply.github.com>
2024-09-22 16:55:28 +0300

    s

--- lua/pitavim/configs/neotree.lua
+++ lua/pitavim/configs/neotree.lua
@@ -42,7 +42,6 @@ require("neo-tree").setup({
 	}),
 
 	source_selector = {
-		winbar = true,
 		statusline = true,
 	},
 	close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
@@ -78,9 +77,9 @@ require("neo-tree").setup({
 			expander_highlight = "NeoTreeExpander",
 		},
 		icon = {
-			folder_closed = "f   ",
-			folder_open = "o   ",
-			folder_empty = "e   ",
+			folder_closed = " ",
+			folder_open = " ",
+			folder_empty = " ",
 			-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 			-- then these will never be used.
 			default = " ",
