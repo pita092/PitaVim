521db43b63a3dab5f76fc245ebfa3316fdc45500
pita <itaymalka37@gmail.com>
2024-09-22 09:55:21 +0300

    s

--- lua/pitavim/configs/lualine.lua
+++ lua/pitavim/configs/lualine.lua
@@ -30,7 +30,7 @@ local diff = {
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "gruvbox light",
+		theme = "solarized",
 		section_separators = { left = " ", right = " " },
 		component_separators = { left = " ", right = " " },
 		disabled_filetypes = { "alpha", "neo-tree" },
