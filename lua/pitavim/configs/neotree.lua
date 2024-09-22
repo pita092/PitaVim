9c66ddc4dc80d52fea102dde1c429a8b3b7a199c
pita <154020297+pita092@users.noreply.github.com>
2024-09-22 16:26:05 +0300

    s

--- lua/pitavim/configs/neotree.lua
+++ lua/pitavim/configs/neotree.lua
@@ -70,7 +70,6 @@ require("neo-tree").setup({
 				-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 				-- then these will never be used.
 				default = " ",
-				highlight = "NeoTreeFileIcon",
 			},
 			modified = {
 				symbol = "[+]",
