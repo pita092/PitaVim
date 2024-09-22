31ff8d86d7bb9cc2b247518363acf6d1e4466e10
pita <154020297+pita092@users.noreply.github.com>
2024-09-22 16:17:28 +0300

    s

--- lua/pitavim/options.lua
+++ lua/pitavim/options.lua
@@ -2,33 +2,37 @@ local opt = vim.opt
 local o = vim.o
 local g = vim.g
 
+opt.hlsearch = false
+opt.incsearch = true
+
 vim.cmd("set number")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.have_nerd_font = true
-vim.opt.relativenumber = true
-vim.opt.mouse = "a"
-vim.opt.showmode = false
+opt.relativenumber = true
+opt.mouse = "a"
+opt.showmode = false
 
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+	opt.clipboard = "unnamedplus"
 end)
 
 vim.g.vimtex_view_method = "mupdf"
 
 o.expandtab = true
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
---vim.opt.listchars = {tab = "» " , trail = "·", nbsp = "␣" }
-vim.opt.listchars = {
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = "yes"
+opt.updatetime = 50
+opt.colorcolumn = "80"
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+--opt.listchars = {tab = "» " , trail = "·", nbsp = "␣" }
+opt.listchars = {
 	eol = "↵",
 	tab = "· ",
 	trail = "▸",
@@ -36,13 +40,13 @@ vim.opt.listchars = {
 	precedes = "❮",
 }
 o.termguicolors = true
-vim.opt.list = true
+opt.list = true
 vim.o.showtabline = 2
 o.updatetime = 50
-vim.opt.inccommand = "split"
+opt.inccommand = "split"
 o.cursorline = false
 o.cursorlineopt = "number"
-vim.opt.scrolloff = 10
+opt.scrolloff = 10
 
 opt.whichwrap:append("<>[]hl")
 
@@ -50,16 +54,6 @@ local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
-
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeFloatNormal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#ffffff", bg = "NONE" })
 
 if vim.g.neovide then
 	vim.g.neovide_padding_top = 0
