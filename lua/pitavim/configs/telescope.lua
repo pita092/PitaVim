8f3412dfd814785ebe34ab1d737497e0c200e8cb
pita <itaymalka37@gmail.com>
2024-09-22 10:18:32 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -126,6 +126,7 @@ require("telescope").setup({
 })
 require("telescope").load_extension("ui-select")
 require("telescope").load_extension("fzf")
+require("telescope").load_extension("media-files")
 
 -- defaults = {
 --   border = false,
