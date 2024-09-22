97e232bd3568fe238556170dc8bc3ffed57e307f
pita <154020297+pita092@users.noreply.github.com>
2024-09-22 16:21:56 +0300

    s

--- lua/pitavim/maps.lua
+++ lua/pitavim/maps.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 vim.keymap.set("n", "<leader>pv", ":Neotree current<CR>", { desc = "File Tree" })
 
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '>-2<CR>gv=gv")
+
 map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
