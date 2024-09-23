1ba749ee5357b48713f86b27cbd7270899d01039
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 16:38:33 +0300

    s

--- lua/pitavim/scripts/colorscheme.lua
+++ lua/pitavim/scripts/colorscheme.lua
@@ -1,10 +1,21 @@
 local M = {}
 
+local function file_exists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
+end
+
+M.colorscheme_file = vim.fn.stdpath("config") .. "/colorscheme.txt"
+
 M.save_colorscheme = function(name)
     vim.g.my_colorscheme = name
     vim.cmd("colorscheme " .. name)
-    -- Save the colorscheme to a file
-    local file = io.open(vim.fn.stdpath("config") .. "/colorscheme.txt", "w")
+    local file = io.open(M.colorscheme_file, "w")
     if file then
         file:write(name)
         file:close()
@@ -12,13 +23,25 @@ M.save_colorscheme = function(name)
 end
 
 M.load_colorscheme = function()
-    local file = io.open(vim.fn.stdpath("config") .. "/colorscheme.txt", "r")
-    if file then
-        local name = file:read("*all")
-        file:close()
-        if name and name ~= "" then
-            vim.cmd("colorscheme " .. name)
+    if file_exists(M.colorscheme_file) then
+        local file = io.open(M.colorscheme_file, "r")
+        if file then
+            local name = file:read("*all")
+            file:close()
+            if name and name ~= "" then
+                vim.cmd("colorscheme " .. name)
+            end
         end
+    else
+        print("Colorscheme file does not exist.")
+    end
+end
+
+M.check_colorscheme_file = function()
+    if file_exists(M.colorscheme_file) then
+        print("Colorscheme file exists at: " .. M.colorscheme_file)
+    else
+        print("Colorscheme file does not exist.")
     end
 end
 
