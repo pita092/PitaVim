9ebaa2092ef2ac6bf15d147a9b1d24d2b4e24029
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 17:56:53 +0300

    s

--- lua/pitavim/scripts/tabline.lua
+++ lua/pitavim/scripts/tabline.lua
@@ -393,7 +393,7 @@ function M.MyTabLine()
 end
 
 function M.ClearHighlight()
-  vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#fabd2f", bold = true })
+  vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#fabd2f", bg = "Normal", bold = true })
   vim.api.nvim_set_hl(0, "TabLine", { fg = "#fbf1c7"})
   vim.api.nvim_set_hl(0, "TabLineSelBorder", { fg = "#fabd2f"})
   vim.api.nvim_set_hl(0, "WarningMsg", { fg = "#fabd2f"})
