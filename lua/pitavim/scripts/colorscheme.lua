b746aad02e773a3a335de999d1a0a278357c3e24
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 16:37:49 +0300

    s

--- lua/pitavim/scripts/colorscheme.lua
+++ lua/pitavim/scripts/colorscheme.lua
@@ -1,14 +1,25 @@
 local M = {}
 
-M.colorscheme = "default" -- Default colorscheme
-
-M.set_colorscheme = function(name)
-  M.colorscheme = name
-  vim.cmd("colorscheme " .. name)
+M.save_colorscheme = function(name)
+    vim.g.my_colorscheme = name
+    vim.cmd("colorscheme " .. name)
+    -- Save the colorscheme to a file
+    local file = io.open(vim.fn.stdpath("config") .. "/colorscheme.txt", "w")
+    if file then
+        file:write(name)
+        file:close()
+    end
 end
 
 M.load_colorscheme = function()
-  vim.cmd("colorscheme " .. M.colorscheme)
+    local file = io.open(vim.fn.stdpath("config") .. "/colorscheme.txt", "r")
+    if file then
+        local name = file:read("*all")
+        file:close()
+        if name and name ~= "" then
+            vim.cmd("colorscheme " .. name)
+        end
+    end
 end
 
 return M
