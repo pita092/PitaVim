bc35bc387b6f0be5b188ec7283af89c0e21d5429
pita <154020297+pita092@users.noreply.github.com>
2024-09-23 21:19:19 +0300

    s

--- lua/pitavim/configs/lsp.lua
+++ lua/pitavim/configs/lsp.lua
@@ -12,9 +12,9 @@ vim.diagnostic.config({
 	},
 })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "single",
-})
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+-- 	border = "single",
+-- })
 
 -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 -- 	border = "single",
