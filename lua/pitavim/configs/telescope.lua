ecd7f612c9b9164bc32b9fda132589c6535c6d21
pita <154020297+pita092@users.noreply.github.com>
2024-09-24 17:44:06 +0300

    s

--- lua/pitavim/configs/telescope.lua
+++ lua/pitavim/configs/telescope.lua
@@ -61,7 +61,7 @@ require("telescope").setup({
 			"--column",
 			"--smart-case",
 		},
-		previewer = true,
+		previewer = false,
 		prompt_prefix = "  ",
 		selection_caret = "   ",
 		entry_prefix = "  ",
