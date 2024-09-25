cf93e458c73d0077e2b4ccce4915903588c163f8
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 20:31:32 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -31,16 +31,18 @@ return {
 			require("nvchad")
 		end,
 	},
-	{
-		"nvim-tree/nvim-web-devicons",
-		lazy = true,
-		opts = function()
-			return { override = require("nvchad.icons.devicons") }
-		end,
-		config = function(_, opts)
-			require("nvim-web-devicons").setup(opts)
-		end,
-	},
+
+	-- {
+	-- 	"nvim-tree/nvim-web-devicons",
+	-- 	lazy = true,
+	-- 	opts = function()
+	-- 		return { override = require("nvchad.icons.devicons") }
+	-- 	end,
+	-- 	config = function(_, opts)
+	-- 		require("nvim-web-devicons").setup(opts)
+	-- 	end,
+	-- },
+	--
 	{
 		"goolord/alpha-nvim",
 		lazy = false,
