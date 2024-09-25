ff90cdc81899e0258b7fe42e68ad1b7ddc0f28ce
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 15:52:17 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -291,7 +291,7 @@ return {
 		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup()
+			require("nvim-treesitter").setup()
 		end,
 	},
 	{
