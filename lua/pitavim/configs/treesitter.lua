de4976c337cc2ed03a3c197ea1925d9e34e23977
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 15:46:20 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -10,9 +10,9 @@ require("nvim-treesitter.configs").setup({
 	incremental_selection = {
 		enable = true,
 		keymaps = {
-			init_selection = "gnn",
+			init_selection = "gnn", -- set to `false` to disable one of the mappings
 			node_incremental = "grn",
-			scopm_incemental = "grc",
+			scope_incremental = "grc",
 			node_decremental = "grm",
 		},
 	},
