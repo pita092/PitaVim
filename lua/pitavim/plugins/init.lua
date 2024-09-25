401d1e915187b1971baa52a8494e54e0f3fd0878
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 17:52:49 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"ziontee113/icon-picker.nvim",
-		cmd = { "IconPickerNormal", "IconPickerYank" },
+		event = "InsertEnter",
 		config = function()
 			require("icon-picker").setup({ disable_legacy_commands = true })
 
