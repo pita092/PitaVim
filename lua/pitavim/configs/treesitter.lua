86ed9a6dc20bd866e167c69d84c9db51eecb97a5
pita <154020297+pita092@users.noreply.github.com>
2024-09-25 15:39:42 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -6,6 +6,12 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 	highlight = { enable = true },
 	indent = { enable = true },
+	keymaps = {
+		-- Your custom capture.
+		["aF"] = "@custom_capture",
+		["af"] = "@function.outer",
+		["if"] = "@function.inner",
+	},
 })
 vim.filetype.add({
 	pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
