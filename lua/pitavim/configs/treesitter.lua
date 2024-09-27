80643b2237cf5e6aec9b07329535af7b92f545fb
pita <154020297+pita092@users.noreply.github.com>
2024-09-27 16:54:49 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: missing-fields
 require("nvim-treesitter.install").compilers = { "gcc" }
 require("nvim-treesitter.configs").setup({
+	sync_install = true,
 	auto_install = true,
 
 	highlight = { enable = true },
