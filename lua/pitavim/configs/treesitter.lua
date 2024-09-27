3573fa1688ac4c4ab71e44c03dec336d7c3e6042
pita <154020297+pita092@users.noreply.github.com>
2024-09-27 16:19:37 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -5,9 +5,20 @@ require("nvim-treesitter.configs").setup({
 	ensure_installed = { "lua", "vim", "vimdoc", "query", "c" },
 	parser_install_dir = parser_install_dir,
 	auto_install = true,
+
 	highlight = { enable = true },
 	indent = { enable = true },
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 })
+
 vim.filetype.add({
 	pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
