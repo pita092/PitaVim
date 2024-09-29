3ed7023e50c93930a9304af28a0c60772bbeddcc
pita <154020297+pita092@users.noreply.github.com>
2024-09-29 22:50:04 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -3,7 +3,7 @@ vim.api.nvim_set_hl(0, "YourCustomHighlightGroup", { fg = "#868686" })
 return {
 	{
 		"Exafunction/codeium.vim",
-		lazy = false,
+		lazy = "BufEnter",
 	},
 	{
 		"rcarriga/nvim-notify",
