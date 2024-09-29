228057950be735e3cdbb195fadcdd6ddf0b10eb2
pita <itaymalka37@gmail.com>
2024-09-29 10:02:25 +0300

    s

--- lua/pitavim/plugins/init.lua
+++ lua/pitavim/plugins/init.lua
@@ -21,7 +21,13 @@ return {
 	},
 	{
 		"Exafunction/codeium.nvim",
-		event = "InsertEnter",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
+		end,
 	},
 
 	--[[clangd]]
