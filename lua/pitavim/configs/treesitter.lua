b1154b50c57980edf4b044077fde6f6a264fb574
pita <itaymalka37@gmail.com>
2024-09-30 11:29:12 +0300

    s

--- lua/pitavim/configs/treesitter.lua
+++ lua/pitavim/configs/treesitter.lua
@@ -37,6 +37,13 @@ require("nvim-treesitter.configs").setup({
 		highlight_definitions = {
 			enable = true,
 			clear_on_cursor_move = true,
+			highlight_current_scope = { enable = true },
+			smart_rename = {
+				enable = true,
+				keymaps = {
+					smart_rename = "<leader>g",
+				},
+			},
 		},
 	},
 })
